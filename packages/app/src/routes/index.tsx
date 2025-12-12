5e97d60d28c4a7303ba5060675c6c8789128659e
rob-myers <me.robmyers@gmail.com>
2025-12-12 11:40:27 +0000

    feat: towards load wasm

--- packages/app/src/routes/index.tsx
+++ packages/app/src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { testLoadWasm } from "@npc-cli/cli/src/test-load-wasm";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -9,6 +10,13 @@ function Index() {
     <>
       <h1 className="text-3xl">My App</h1>
       <div>Hello, world!</div>
+      <button
+        type="button"
+        className="border rounded-xl px-4 py-2 bg-gray-100 cursor-pointer hover:brightness-125"
+        onClick={testLoadWasm}
+      >
+        Test load wasm
+      </button>
     </>
   );
 }
---
+++ packages/cli/src/test-load-wasm.ts
@@ -0,0 +1,8 @@
+import "../vendors/wasm_exec.js";
+
+export async function testLoadWasm() {
+  alert("loading...");
+  // 🚧
+  // const go = new Go();
+  // console.log({ go });
+}
