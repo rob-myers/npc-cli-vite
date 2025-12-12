88aec4fa43540afdd2f4bc1f957d4c3bf83bf793
rob-myers <me.robmyers@gmail.com>
2025-12-12 14:08:12 +0000

    feat: can load wasm instance

--- packages/cli/src/test-load-wasm.ts
+++ packages/cli/src/test-load-wasm.ts
@@ -1,8 +1,30 @@
-import "../vendors/wasm_exec.js";
+import "../vendors/wasm_exec";
 
+/**
+ * https://tinygo.org/docs/guides/webassembly/wasm/
+ */
 export async function testLoadWasm() {
   alert("loading...");
+
+  const go = new Go();
+
+  const WASM_URL = new URL("../main.wasm", import.meta.url).href;
+
+  const wasm = await (async () =>
+    "instantiateStreaming" in WebAssembly
+      ? WebAssembly.instantiateStreaming(fetch(WASM_URL), go.importObject).then((obj) => {
+          go.run(obj.instance);
+          return obj.instance;
+        })
+      : fetch(WASM_URL)
+          .then((resp) => resp.arrayBuffer())
+          .then((bytes) =>
+            WebAssembly.instantiate(bytes, go.importObject).then((obj) => {
+              go.run(obj.instance);
+              return obj.instance;
+            }),
+          ))();
+
   // 🚧
-  // const go = new Go();
-  // console.log({ go });
+  console.log({ wasm });
 }
