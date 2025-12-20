c56301bdb72b8f4c074e202beca50f8ec9817728
rob-myers <me.robmyers@gmail.com>
2025-12-20 16:42:29 +0000

    feat: useResponsiveLayout

--- packages/app/src/routes/index.tsx
+++ packages/app/src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Responsive as ResponsiveReactGridLayout, useContainerWidth } from "react-grid-layout";
+import { GridLayout, type Layout, useContainerWidth, useResponsiveLayout } from "react-grid-layout";
 
 import "react-grid-layout/css/styles.css";
 import "react-resizable/css/styles.css";
@@ -16,16 +16,34 @@ function Index() {
   const { width, containerRef, mounted } = useContainerWidth();
   const theme = useThemeName();
 
+  const {
+    layout, // Current layout for active breakpoint
+    cols, // Column count for current breakpoint
+    // layouts, // All layouts by breakpoint
+    // breakpoint, // Current active breakpoint ('lg', 'md', etc.)
+    // setLayoutForBreakpoint,
+    // setLayouts,
+    // sortedBreakpoints,
+  } = useResponsiveLayout({
+    width,
+    breakpoints: demo.breakpoints,
+    cols: demo.cols,
+    layouts: demo.layouts,
+    compactType: "vertical",
+    onBreakpointChange: (bp, cols) => console.log(`Now at ${bp} (${cols} cols)`),
+    // onLayoutChange: (layout, allLayouts) => saveToServer(allLayouts)
+  });
+
   return (
     <div ref={containerRef} className="w-full">
       {mounted && (
-        <ResponsiveReactGridLayout
+        <GridLayout
           className="border text-on-background [&_.react-resizable-handle::after]:border-on-background!"
-          // gridConfig={{ cols: 12, rowHeight: 50 }}
-          breakpoints={breakpoints}
-          layouts={layouts}
-          cols={cols}
           width={width}
+          gridConfig={{
+            cols, // 🔔 not mentioned in documentation
+          }}
+          layout={layout}
         >
           {["a", "b", "c"].map((key) => (
             <div key={key} className="border flex items-center justify-center">
@@ -44,26 +62,27 @@ function Index() {
           <div key="e" className="border p-4 flex items-center">
             <button
               type="button"
-              className="cursor-pointer"
-              // onClick={themeApi.setOther}>
-              onPointerDown={themeApi.setOther}
+              className="cursor-pointer border rounded px-4 py-1"
+              onPointerUp={themeApi.setOther}
             >
               {theme}
             </button>
           </div>
-        </ResponsiveReactGridLayout>
+        </GridLayout>
       )}
     </div>
   );
 }
 
-const layouts = {
-  lg: [
-    { i: "a", x: 0, y: 0, w: 1, h: 2, static: false },
-    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-    { i: "c", x: 4, y: 0, w: 1, h: 2 },
-    { i: "d", x: 0, y: 1, w: 4, h: 4 },
-  ],
+const demo = {
+  layouts: {
+    lg: [
+      { i: "a", x: 0, y: 0, w: 1, h: 2, static: false },
+      { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+      { i: "c", x: 4, y: 0, w: 1, h: 2 },
+      { i: "d", x: 0, y: 1, w: 4, h: 4, isDraggable: false },
+    ],
+  } satisfies Partial<Record<"lg" | "sm", Layout>> as Partial<Record<"lg" | "sm", Layout>>,
+  breakpoints: { lg: 1200, sm: 768 },
+  cols: { lg: 12, sm: 6 },
 };
-const breakpoints = { lg: 1200, sm: 768 };
-const cols = { lg: 12, sm: 6 };
