ceda74353f5b9d64479518529168bf741bca2f00
rob-myers <me.robmyers@gmail.com>
2025-12-28 18:27:38 +0100

    chore: rename

--- packages/parse-sh/src/main.ts
+++ packages/parse-sh/src/main.ts
@@ -2,4 +2,4 @@ export type { JSh } from "./jsh.d";
 export * from "./jsh.model";
 export type { MvdanSh } from "./mvdan-sh.d";
 export * from "./parse";
-export * from "./test-load-wasm";
+export * from "./test-wasm";
