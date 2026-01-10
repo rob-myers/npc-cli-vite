6d95a209a4eae44ce7f0e05c6f984eb9c64039ff
rob-myers <me.robmyers@gmail.com>
2026-01-10 12:24:32 +0000

    feat: hook up ui Global

--- packages/ui/registry/src/main.ts
+++ packages/ui/registry/src/main.ts
@@ -1,9 +1,11 @@
 import { Blog } from "@npc-cli/ui__blog";
+import { Global } from "@npc-cli/ui__global";
 import { Jsh } from "@npc-cli/ui__jsh";
 import { Template } from "@npc-cli/ui__template";
 
 export const uiRegistry = {
   Blog,
+  Global,
   Jsh,
   Template,
 };
