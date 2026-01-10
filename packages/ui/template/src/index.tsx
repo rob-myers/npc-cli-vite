0f6b230584b65c5c75fed2ec29de8b1b23e54f61
rob-myers <me.robmyers@gmail.com>
2026-01-10 18:03:32 +0000

    feat: tweak template component

--- packages/ui/template/src/index.tsx
+++ packages/ui/template/src/index.tsx
@@ -1,5 +1,9 @@
 export function Template() {
   return (
-    <div className="bg-white text-black rounded px-2 py-1 text-center">Template Component</div>
+    <div className="overflow-auto size-full flex justify-center items-center">
+      <div className="bg-white/80 text-black/70 rounded px-4 py-2 text-center leading-4 transition-transform hover:scale-125 cursor-pointer">
+        Template Component
+      </div>
+    </div>
   );
 }
