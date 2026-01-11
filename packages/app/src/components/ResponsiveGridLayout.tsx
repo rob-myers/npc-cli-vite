80c96665c7cab8a8d7b5f11975bd50334c6de0c4
rob-myers <me.robmyers@gmail.com>
2026-01-11 13:34:48 +0000

    chore: tidy ResponsiveGridLayout into useStateRef

--- packages/app/src/components/ResponsiveGridLayout.tsx
+++ packages/app/src/components/ResponsiveGridLayout.tsx
@@ -1,7 +1,9 @@
 import { type UiRegistryKey, uiRegistry } from "@npc-cli/ui__registry";
-import { cn } from "@npc-cli/util";
-import React, { useEffect, useRef, useState } from "react";
+import { cn, useStateRef, useUpdate } from "@npc-cli/util";
+import { pause } from "@npc-cli/util/legacy/generic";
+import React, { useEffect, useRef } from "react";
 import { GridLayout, type Layout, useContainerWidth, useResponsiveLayout } from "react-grid-layout";
+import type { GridConfig } from "react-grid-layout/core";
 
 import "react-grid-layout/css/styles.css";
 import "react-resizable/css/styles.css";
@@ -26,41 +28,58 @@ export function ResponsiveGridLayout({
     },
   });
 
-  const [preventTransition, setPreventTransition] = useState(true);
-  const [resizing, setResizing] = useState(false);
-  const [dragging, setDragging] = useState(false);
+  const state = useStateRef(
+    (): State => ({
+      preventTransition: true,
+      resizing: false,
+      dragging: false,
+      gridConfig: {
+        cols,
+        rowHeight: 80,
+        // margin: [0, 0],
+      },
+      onMount() {
+        pause(1).then(() => state.set({ preventTransition: false }));
+      },
+      onResizeStart() {
+        state.set({ resizing: true });
+      },
+      onResizeStop() {
+        layouts.current[breakpoint] = layout;
+        state.set({ resizing: false });
+      },
+      onDragStart() {
+        state.set({ dragging: true });
+      },
+      onDragStop() {
+        layouts.current[breakpoint] = layout;
+        state.set({ dragging: false });
+      },
+      set(partial: Partial<State>) {
+        Object.assign(state, partial);
+        update();
+      },
+    }),
+  );
+  const update = useUpdate();
 
-  useEffect(() => void setTimeout(() => setPreventTransition(false), 1), []);
+  useEffect(state.onMount, []);
 
   return (
     <div ref={containerRef} className="w-full overflow-auto h-full border border-white">
       <GridLayout
         className={cn(
-          preventTransition && "[&_.react-grid-item]:transition-none!",
-          (resizing || dragging) && "select-none",
+          state.preventTransition && "[&_.react-grid-item]:transition-none!",
+          (state.resizing || state.dragging) && "select-none",
           "text-on-background/60 [&_.react-resizable-handle::after]:border-on-background!",
         )}
         width={width}
-        gridConfig={{
-          cols,
-          rowHeight: 80,
-          // margin: [0, 0],
-        }}
+        gridConfig={state.gridConfig}
         layout={layout}
-        onResizeStart={() => {
-          setResizing(true);
-        }}
-        onResizeStop={(layout) => {
-          layouts.current[breakpoint] = layout;
-          setResizing(false);
-        }}
-        onDragStart={() => {
-          setDragging(true);
-        }}
-        onDragStop={(layout) => {
-          layouts.current[breakpoint] = layout;
-          setDragging(false);
-        }}
+        onResizeStart={state.onResizeStart}
+        onResizeStop={state.onResizeStop}
+        onDragStart={state.onDragStart}
+        onDragStop={state.onDragStop}
       >
         {layout.map((item) => (
           <div key={item.i} className="border rounded *:rounded">
@@ -81,4 +100,17 @@ export type UiLayout = {
   cols: Record<"lg" | "sm", number>;
   layouts: Record<"lg" | "sm", Layout>;
   layoutToUi: { [layoutKey: string]: { uiKey: UiRegistryKey } };
+};
+
+type State = {
+  preventTransition: boolean;
+  resizing: boolean;
+  dragging: boolean;
+  gridConfig: Partial<GridConfig>;
+  onMount(): void;
+  onResizeStart(): void;
+  onResizeStop(): void;
+  onDragStart(): void;
+  onDragStop(): void;
+  set(partial: Partial<State>): void;
 };
