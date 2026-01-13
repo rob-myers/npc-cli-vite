cda4056debb125f1cf71cd318b55eb136495a99b
rob-myers <me.robmyers@gmail.com>
2026-01-13 16:08:37 +0000

    chore: abstract preload grid skeleton

---
+++ packages/app/src/components/PreloadGrid.tsx
@@ -0,0 +1,30 @@
+import { motion } from "motion/react";
+import { useStore } from "zustand/react";
+import { layoutStore } from "./layout-store";
+
+/**
+ * Improve initial rendering by showing a skeleton of the grid layout
+ */
+export const PreloadGrid = () => {
+  const { itemToRect, ready } = useStore(layoutStore);
+
+  return (
+    <motion.div
+      className="fixed pointer-events-none"
+      initial={{ opacity: 1 }}
+      animate={ready ? { opacity: 0, transition: { duration: 1 } } : undefined}
+    >
+      {Object.entries(itemToRect).map(([itemId, { x, y, width, height }]) => (
+        <div
+          key={itemId}
+          className="absolute border border-white/30"
+          style={{
+            width,
+            height,
+            transform: `translate3d(${x}px, ${y}px, 0)`,
+          }}
+        ></div>
+      ))}
+    </motion.div>
+  );
+};
