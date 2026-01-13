eea747e7e1fddcfe757d90b09ad91a8fc0d3d518
rob-myers <me.robmyers@gmail.com>
2026-01-13 17:34:04 +0000

    feat: create ui/world + fix root tsconfig

--- packages/ui/registry/src/main.ts
+++ packages/ui/registry/src/main.ts
@@ -5,6 +5,9 @@ export const uiRegistry = {
   Global: lazy(() => import("@npc-cli/ui__global")),
   Jsh: lazy(() => import("@npc-cli/ui__jsh")),
   Template: lazy(() => import("@npc-cli/ui__template")),
+  World: lazy(() => import("@npc-cli/ui__world")),
 };
 
 export type UiRegistryKey = keyof typeof uiRegistry;
+
+export const uiRegistryKeys = Object.keys(uiRegistry) as UiRegistryKey[];
---
+++ packages/ui/world/src/index.tsx
@@ -0,0 +1,9 @@
+export function World() {
+  return (
+    <div className="overflow-auto size-full flex justify-center items-center">
+      <div className="bg-white/80 text-black/70 rounded px-4 py-2 text-center leading-4 transition-transform hover:scale-125 cursor-pointer">
+        World Component
+      </div>
+    </div>
+  );
+}
