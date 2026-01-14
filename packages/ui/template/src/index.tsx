7825829b834f88a84eb5f4f4eb6e2d154c225db4
rob-myers <me.robmyers@gmail.com>
2026-01-14 16:43:00 +0000

    feat: restyle template component button

--- packages/ui/template/src/index.tsx
+++ packages/ui/template/src/index.tsx
@@ -1,7 +1,7 @@
 export function Template() {
   return (
     <div className="overflow-auto size-full flex justify-center items-center">
-      <div className="bg-white/80 text-black/70 rounded px-4 py-2 text-center leading-4 transition-transform hover:scale-125 cursor-pointer">
+      <div className="bg-button text-on-background/70 border rounded px-4 py-2 text-center leading-4 transition-transform hover:scale-125 cursor-pointer">
         Template Component
       </div>
     </div>
