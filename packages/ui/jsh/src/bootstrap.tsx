7bef9b624e953e471e8ab53b8112f9542c9482a1
rob-myers <me.robmyers@gmail.com>
2026-01-21 17:25:36 +0000

    chore: clean UiInstanceMenu

--- packages/ui/jsh/src/bootstrap.tsx
+++ packages/ui/jsh/src/bootstrap.tsx
@@ -1,11 +1,14 @@
 import type { UiBootstrapProps } from "@npc-cli/ui-sdk";
 
-// 🚧
 export default function JshBootstrap(props: UiBootstrapProps): React.ReactNode {
   return (
     <div className="border flex w-full bg-black text-white">
-      <label className="flex">
-        <input type="text" className="w-full p-1 border border-black/30" placeholder="sessionKey" />
+      <label className="flex border p-0.5 w-32">
+        <input
+          type="text"
+          className="w-full p-1 border border-black/30 outline-black"
+          placeholder="sessionKey"
+        />
       </label>
       <button type="button" className="p-2  text-sm" onClick={props.addInstance}>
         Create
