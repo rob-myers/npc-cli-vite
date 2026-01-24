4d6a40883338c55ffed51f070e3125b0ca2ce88d
rob-myers <me.robmyers@gmail.com>
2026-01-24 15:38:12 +0000

    feat: example of cleaner UI using state.set(partial)

--- packages/ui/jsh/src/bootstrap.tsx
+++ packages/ui/jsh/src/bootstrap.tsx
@@ -1,5 +1,5 @@
 import { type UiBootstrapProps, UiContext } from "@npc-cli/ui-sdk";
-import { useStateRef, useUpdate } from "@npc-cli/util";
+import { cn, useStateRef } from "@npc-cli/util";
 import { useContext } from "react";
 
 export default function JshBootstrap(props: UiBootstrapProps): React.ReactNode {
@@ -9,34 +9,34 @@ export default function JshBootstrap(props: UiBootstrapProps): React.ReactNode {
   // 🚧 validate sessionKey `tty-{n}`
 
   const state = useStateRef(() => ({
+    invalid: false,
     sessionKey: "tty-0",
   }));
-  const update = useUpdate();
 
   return (
     <div className="border flex w-full bg-black text-white">
       <label className="flex border p-0.5 w-32">
         <input
           type="text"
-          className="w-full p-1 border border-black/30 outline-black"
+          className={cn("w-full p-1 border border-black/30 invalid:bg-red-400/30 outline-black")}
           placeholder="sessionKey"
-          onChange={(e) => {
-            state.sessionKey = e.target.value;
-            update();
-          }}
+          onChange={(e) => state.set({ sessionKey: e.currentTarget.value })}
+          onInput={(e) => state.set({ invalid: !e.currentTarget.checkValidity() })}
+          pattern="tty-[0-9]+"
           value={state.sessionKey}
         />
       </label>
       <button
         type="button"
-        className="p-2  text-sm"
+        className="p-2 text-sm"
+        disabled={state.invalid}
         onClick={() => {
           props.addInstance({
             sessionKey: state.sessionKey,
           });
         }}
       >
-        Create
+        {state.invalid ? "❌" : "Create"}
       </button>
     </div>
   );
