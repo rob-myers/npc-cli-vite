c3597604b2b70203fff67519afbc9a1c91d61c7e
rob-myers <me.robmyers@gmail.com>
2026-01-25 16:51:24 +0000

    feat: support `pnpm dev-hotspot` with ssl for crypto

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,6 +1,7 @@
 import mdx from "@mdx-js/rollup";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
@@ -15,5 +16,7 @@ export default defineConfig({
     mdx(),
     react(),
     tailwindcss(),
+    // pnpm dev-hotspot needs https for crypto
+    process.env.USE_HTTPS ? basicSsl() : undefined,
   ],
 });
