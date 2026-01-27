5e6febd9433cdcf0f63ba886327aea74bed74ad1
rob-myers <me.robmyers@gmail.com>
2026-01-27 19:49:09 +0000

    feat: uis have schemas

--- packages/ui/jsh/src/index.ts
+++ packages/ui/jsh/src/index.ts
@@ -1,9 +1,17 @@
-import { defineUi } from "@npc-cli/ui-sdk";
+import { BaseUiMetaSchema, defineUi } from "@npc-cli/ui-sdk";
 import { lazy } from "react";
-
+import z from "zod";
 import JshBootstrap from "./bootstrap";
+
+const schema = z.object({
+  ...BaseUiMetaSchema.shape,
+  sessionKey: z.templateLiteral(["tty-", z.number()]),
+});
 
 export default defineUi({
   ui: lazy(() => import("./Jsh")),
   bootstrap: JshBootstrap,
+  schema,
 });
+
+export type JshUiMeta = z.infer<typeof schema>;
