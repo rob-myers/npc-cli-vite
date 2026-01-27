b884ef0e4b6ea65d34604b10bbb180963ffc1e2d
rob-myers <me.robmyers@gmail.com>
2026-01-27 18:12:16 +0000

    feat: start extending World

--- packages/ui/world/src/World.tsx
+++ packages/ui/world/src/World.tsx
@@ -1,11 +1,26 @@
 import type { UiProps } from "@npc-cli/ui-sdk";
+import { useStateRef } from "@npc-cli/util";
+import { WorldContext } from "./world-context";
 
 export default function World(_props: UiProps) {
+  const state = useStateRef<State>(() => ({
+    // 🚧 derived from props
+    key: "world-0",
+    disabled: false,
+  }));
+
   return (
-    <div className="overflow-auto size-full flex justify-center items-center">
-      <div className="bg-white/80 text-black/70 rounded px-4 py-2 text-center leading-4 transition-transform hover:scale-125 cursor-pointer">
-        World Component
+    <WorldContext.Provider value={state}>
+      <div className="overflow-auto size-full flex justify-center items-center">
+        <div className="bg-white/80 text-black/70 rounded px-4 py-2 text-center leading-4 transition-transform hover:scale-125 cursor-pointer">
+          World Component
+        </div>
       </div>
-    </div>
+    </WorldContext.Provider>
   );
 }
+
+export type State = {
+  key: `world-${number}`;
+  disabled: boolean;
+};
