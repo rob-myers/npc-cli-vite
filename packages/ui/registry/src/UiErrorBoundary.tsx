7b6d44cba9655883fc2b81581a349249bf0e2387
rob-myers <me.robmyers@gmail.com>
2026-01-28 16:17:51 +0000

    feat: UiErrorBoundary

---
+++ packages/ui/registry/src/UiErrorBoundary.tsx
@@ -0,0 +1,33 @@
+import { Component } from "react";
+
+export class UiErrorBoundary extends Component<
+  React.PropsWithChildren,
+  { error: Error | typeof NoErrorSymbol }
+> {
+  constructor(props: React.PropsWithChildren) {
+    super(props);
+    this.state = { error: NoErrorSymbol };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return { error };
+  }
+
+  override render() {
+    if (this.state.error === NoErrorSymbol) {
+      return this.props.children;
+    }
+    return (
+      <div className="flex flex-col gap-1 text-on-background">
+        <h2 className="p-2 border-b border-on-background/25 bg-[repeating-linear-gradient(45deg,var(--pattern-fg)_0,var(--pattern-fg)_1px,transparent_0,transparent_50%)] bg-[size:10px_10px] bg-fixed [--pattern-fg:color-mix(in_oklch,var(--color-on-background)_20%,transparent)]">
+          UI Error
+        </h2>
+        <pre className="px-2 whitespace-pre-wrap font-sans text-sm leading-relaxed tracking-wide">
+          {this.state.error.message}
+        </pre>
+      </div>
+    );
+  }
+}
+
+const NoErrorSymbol = Symbol();
