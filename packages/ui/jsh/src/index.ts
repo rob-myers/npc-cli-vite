688f4e977aee2db8733431328613f20a058e40c7
rob-myers <me.robmyers@gmail.com>
2026-01-28 13:26:44 +0000

    feat: World has bootstrap ui to provide worldKey

--- packages/ui/jsh/src/index.ts
+++ packages/ui/jsh/src/index.ts
@@ -1,6 +1,6 @@
 import { defineUi } from "@npc-cli/ui-sdk";
 import { lazy } from "react";
-import JshBootstrap from "./bootstrap";
+import { JshBootstrap } from "./bootstrap";
 import { JshUiSchema } from "./schema";
 
 export default defineUi({
--- packages/ui/world/src/index.ts
+++ packages/ui/world/src/index.ts
@@ -1,9 +1,10 @@
 import { defineUi } from "@npc-cli/ui-sdk";
 import { lazy } from "react";
+import WorldBootstrap from "./bootstrap";
 import { WorldUiSchema } from "./schema";
 
 export default defineUi({
   ui: lazy(() => import("./World")),
-  bootstrap: null,
+  bootstrap: WorldBootstrap,
   schema: WorldUiSchema,
 });
