350bb82b92884fb900b927b3818bcd2aa6a36f39
rob-myers <me.robmyers@gmail.com>
2026-01-29 12:22:30 +0000

    feat: extend ui/template + util/types provides type-fest

--- packages/ui/tabs/src/Template.tsx
+++
@@ -1,9 +0,0 @@
-export default function Template() {
-  return (
-    <div className="overflow-auto size-full flex justify-center items-center">
-      <div className="bg-button-background text-on-background/70 border rounded px-4 py-2 text-center leading-4 transition-transform hover:scale-125 cursor-pointer">
-        Template Component
-      </div>
-    </div>
-  );
-}
