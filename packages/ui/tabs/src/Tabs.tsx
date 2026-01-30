d5fc2541a83e126c18a67630870732f80ee078c1
rob-myers <me.robmyers@gmail.com>
2026-01-30 17:12:55 +0000

    feat: fix basic tabs ui + basic add tab

--- packages/ui/tabs/src/Tabs.tsx
+++ packages/ui/tabs/src/Tabs.tsx
@@ -1,12 +1,16 @@
-import { UiContext } from "@npc-cli/ui-sdk";
+import { UiContext, uiStore } from "@npc-cli/ui-sdk";
 import { cn } from "@npc-cli/util";
 import { pause } from "@npc-cli/util/legacy/generic";
 import { PlusCircleIcon } from "@phosphor-icons/react";
 import type { ReactNode } from "react";
 import { useContext, useRef, useState } from "react";
-import type { TabsUiMeta } from "./schema";
+import type { TabsUiMeta, TabUiMeta } from "./schema";
 
-export default function Tabs({ meta: { items } }: { meta: TabsUiMeta }): ReactNode {
+export default function Tabs({
+  meta: { layoutId: rootId, items },
+}: {
+  meta: TabsUiMeta;
+}): ReactNode {
   const { layoutApi } = useContext(UiContext);
   const [activeKey, setActiveKey] = useState(items[0]?.layoutId); // 🚧 layoutId -> id?
 
@@ -20,10 +24,8 @@ export default function Tabs({ meta: { items } }: { meta: TabsUiMeta }): ReactNo
           <button
             key={tab.layoutId}
             className={cn(
-              "px-4 py-2 -mb-px border-b-2 border-outline font-medium focus:outline-none transition-colors duration-200 bg-background",
-              activeKey === tab.layoutId
-                ? "border-primary text-primary"
-                : "border-outline text-on-background hover:text-primary/80",
+              "cursor-pointer px-4 py-2 -mb-px border-b-2 border-outline font-medium focus:outline-none transition-colors duration-200 bg-background",
+              activeKey !== tab.layoutId && "opacity-50 hover:opacity-80",
             )}
             onClick={() => setActiveKey(tab.layoutId)}
             type="button"
@@ -41,9 +43,18 @@ export default function Tabs({ meta: { items } }: { meta: TabsUiMeta }): ReactNo
 
             layoutApi.overrideContextMenu({
               refObject: newTabButtonRef,
-              addItem({ uiMeta, itemId, gridRect }) {
+              addItem({ uiMeta, itemId }) {
                 // 🚧
-                alert(`Add tab: ${JSON.stringify({ uiMeta, itemId, gridRect })}`);
+                // 🚧 zod parse rootMeta and uiMeta
+                const rootMeta = uiStore.getState().metaById[rootId];
+                console.log("Add tab", { rootMeta, uiMeta, itemId });
+                uiStore.setState((draft) => {
+                  (draft.metaById[rootId] as TabsUiMeta).items.push({
+                    layoutId: itemId,
+                    title: (uiMeta as TabUiMeta).title,
+                    uiKey: uiMeta.uiKey,
+                  });
+                });
               },
             });
           }}
@@ -51,7 +62,7 @@ export default function Tabs({ meta: { items } }: { meta: TabsUiMeta }): ReactNo
           <PlusCircleIcon className="size-4" weight="duotone" />
         </button>
       </div>
-      <div className="pt-4 flex-1">
+      <div className="pt-4 px-2 flex-1 size-full overflow-auto">
         {items.map((tab) => (
           <div
             key={tab.layoutId}
