bdedb5c3f92edb76aa78eaa9473a81fb3da14401
rob-myers <me.robmyers@gmail.com>
2026-01-31 17:26:38 +0000

    feat: tabs have submenu

--- packages/ui/tabs/src/Tabs.tsx
+++ packages/ui/tabs/src/Tabs.tsx
@@ -1,7 +1,12 @@
 import { UiContext, UiInstance, uiStore } from "@npc-cli/ui-sdk";
-import { cn } from "@npc-cli/util";
+import { BasicPopover, cn } from "@npc-cli/util";
 import { pause } from "@npc-cli/util/legacy/generic";
-import { PlusCircleIcon } from "@phosphor-icons/react";
+import {
+  ArrowUpRightIcon,
+  DotsThreeOutlineVerticalIcon,
+  PlusCircleIcon,
+  TrashIcon,
+} from "@phosphor-icons/react";
 import type { ReactNode } from "react";
 import { useContext, useRef } from "react";
 import type { TabsUiMeta } from "./schema";
@@ -12,30 +17,61 @@ export default function Tabs({ meta }: { meta: TabsUiMeta }): ReactNode {
 
   return (
     <div className={cn("flex flex-col size-full overflow-auto")}>
-      <div className="flex border-b border-outline">
-        {meta.items.length === 0 && <div className="p-2">Empty tabs...</div>}
+      <div className="flex items-center border-b border-outline">
+        {meta.items.length === 0 && <div className="p-2 italic">no tabs</div>}
         {meta.items.map((tab) => (
           <button
             key={tab.id}
+            type="button"
             className={cn(
-              "cursor-pointer px-4 py-2 -mb-px border-b-2 border-outline font-medium text-sm focus:outline-none bg-background",
+              "flex gap-2",
+              "cursor-pointer px-1 pt-2 -mb-px border-b-2 border-outline font-medium text-sm focus:outline-none",
               meta.currentTabId !== tab.id && "opacity-50 hover:opacity-80",
             )}
             onClick={() =>
+              // 🚧 reparse tabs meta
               uiStore.setState((draft) => {
-                // 🚧 reparse tabs meta
                 (draft.metaById[meta.id] as TabsUiMeta).currentTabId = tab.id;
               })
             }
-            type="button"
           >
-            <pre>{tab.title}</pre>
+            <div className="flex p-1 border border-on-background/20">
+              <pre className="p-1">{tab.title}</pre>
+
+              <BasicPopover
+                trigger={
+                  <DotsThreeOutlineVerticalIcon
+                    weight="thin"
+                    className="cursor-pointer size-4 text-on-background/80"
+                  />
+                }
+                className="bg-gray-600/70 p-1 pt-2"
+                arrowClassName="fill-gray-600/70"
+                side="bottom"
+              >
+                <div className="flex gap-2">
+                  <button type="button">
+                    <ArrowUpRightIcon
+                      weight="thin"
+                      className="cursor-pointer size-5 bg-black/40 text-white"
+                    />
+                  </button>
+
+                  <button type="button">
+                    <TrashIcon
+                      weight="thin"
+                      className="cursor-pointer size-5 bg-black/40 text-white"
+                    />
+                  </button>
+                </div>
+              </BasicPopover>
+            </div>
           </button>
         ))}
         <button
           ref={newTabButtonRef}
           type="button"
-          className="cursor-pointer open-context-menu"
+          className="cursor-pointer pl-2 pt-2"
           onPointerUp={(e) => {
             e.stopPropagation();
             pause(30); // avoid immediate select context menu item
@@ -62,7 +98,7 @@ export default function Tabs({ meta }: { meta: TabsUiMeta }): ReactNode {
             });
           }}
         >
-          <PlusCircleIcon className="size-4" weight="duotone" />
+          <PlusCircleIcon className="size-5" weight="duotone" />
         </button>
       </div>
 
