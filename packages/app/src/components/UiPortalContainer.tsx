6eb52e14a636bd42b965890283da646b53736acf
rob-myers <me.robmyers@gmail.com>
2026-02-03 10:46:48 +0000

    feat: remove uiStore toInitMeta

--- packages/app/src/components/UiPortalContainer.tsx
+++ packages/app/src/components/UiPortalContainer.tsx
@@ -1,37 +1,12 @@
 import { uiRegistry } from "@npc-cli/ui-registry";
-import { HtmlPortalWrapper, UiErrorBoundary, UiParseError, uiStore } from "@npc-cli/ui-sdk";
+import { UiErrorBoundary, UiParseError, uiStore } from "@npc-cli/ui-sdk";
 import { Spinner } from "@npc-cli/util";
-import { castDraft } from "immer";
-import { Suspense, useEffect } from "react";
+import { Suspense } from "react";
 import * as portals from "react-reverse-portal";
 import { useStore } from "zustand";
 
 export const UiPortalContainer = () => {
-  const toInitMeta = useStore(uiStore, (state) => state.toInitMeta);
   const byId = useStore(uiStore, (state) => state.byId);
-
-  // make byId reflect toInitMeta
-  useEffect(() => {
-    uiStore.setState((draft) => {
-      for (const [id, initMeta] of Object.entries(toInitMeta)) {
-        if (!draft.byId[id]) {
-          const def = uiRegistry[initMeta.uiKey];
-          const result = def.schema.safeParse(initMeta);
-
-          draft.byId[id] = {
-            meta: result.success ? result.data : initMeta,
-            portal: castDraft(new HtmlPortalWrapper()),
-            zodError: result.success ? undefined : castDraft(result.error),
-          };
-        }
-      }
-      for (const [id] of Object.entries(draft.byId)) {
-        if (!toInitMeta[id]) {
-          delete draft.byId[id];
-        }
-      }
-    });
-  }, [toInitMeta]);
 
   return (
     <div className="hidden">
