d5b43a19e3123b828aca3088c90f22cf3ef43ab4
rob-myers <me.robmyers@gmail.com>
2026-02-06 14:18:26 +0000

    feat: media will export urls

--- packages/media/src/index.ts
+++ packages/media/src/index.ts
@@ -1,3 +1,7 @@
-export const meta = {
-  info: "media goes here",
+import testBlockBench5Gltf from "./blockbench/TestBlockbench5.gltf?url";
+
+import "./gltf.d";
+
+export const url = {
+  testBlockBench5Gltf,
 };
