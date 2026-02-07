ca14b10ba9fa8a722e1a0a85446685104af171ea
rob-myers <me.robmyers@gmail.com>
2026-02-07 19:44:43 +0000

    feat: hook up human-0 gltf

--- packages/media/src/index.ts
+++ packages/media/src/index.ts
@@ -1,7 +1,9 @@
+import humanZeroGltf from "./blockbench/human-0.gltf?url";
 import testBlockBench5Gltf from "./blockbench/TestBlockbench5.gltf?url";
 
 import "./gltf.d";
 
 export const url = {
   testBlockBench5Gltf,
+  humanZeroGltf,
 };
