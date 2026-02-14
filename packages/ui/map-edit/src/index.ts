34a3e79fe088c9bde20f3204861376d49fd559a2
rob-myers <me.robmyers@gmail.com>
2026-02-14 18:34:59 +0000

    feat: towards dnd mobile

--- packages/ui/map-edit/src/index.ts
+++ packages/ui/map-edit/src/index.ts
@@ -2,6 +2,8 @@ import { defineUi } from "@npc-cli/ui-sdk";
 import { lazy } from "react";
 import { MapEditUiMetaSchema } from "./schema";
 
+import "./@dragdroptouch__drag-drop-touch.d";
+
 export default defineUi({
   ui: lazy(() => import("./MapEdit")),
   bootstrap: null,
