29c7d61cd4f3f5aecb3534924681092206e4118f
rob-myers <me.robmyers@gmail.com>
2026-02-14 13:41:48 +0000

    feat: UiInstanceMenu in ui-sdk and optionally rendered via optional boolean meta.customUiInstanceMenu

--- packages/app/src/components/UiGrid.tsx
+++ packages/app/src/components/UiGrid.tsx
@@ -6,13 +6,13 @@ import {
   type OverrideContextMenuOpts,
   type UiBootstrapProps,
   type UiContextValue,
+  UiInstanceMenu,
   type UiInstanceMeta,
   uiStore,
   uiStoreApi,
 } from "@npc-cli/ui-sdk";
 import {
   allowReactGridDragClassName,
-  BasicPopover,
   cn,
   PopoverArrow,
   preventReactGridDragClassName,
@@ -21,7 +21,7 @@ import {
 } from "@npc-cli/util";
 import { isTouchDevice } from "@npc-cli/util/legacy/dom";
 import { mapValues, pause } from "@npc-cli/util/legacy/generic";
-import { LayoutIcon, LockIcon, PenIcon, PlayCircleIcon, XIcon } from "@phosphor-icons/react";
+import { LockIcon, PenIcon } from "@phosphor-icons/react";
 import type React from "react";
 import { Suspense, useEffect, useMemo, useRef } from "react";
 import { useBeforeunload } from "react-beforeunload";
@@ -322,7 +322,7 @@ export function UiGrid({ extendContextValue, persistedLayout }: Props) {
                     )}
                   >
                     <portals.OutPortal node={portal.portalNode} />
-                    <UiInstanceMenu meta={meta} />
+                    {!meta.customUiInstanceMenu && <UiInstanceMenu meta={meta} />}
                     <DraggableOverlay />
                   </div>
                 );
@@ -492,60 +492,3 @@ type State = {
   persist(): void;
   updateNumTouches(e: React.TouchEvent<HTMLElement>): void;
 };
-
-function UiInstanceMenu({ meta }: { meta: UiInstanceMeta }) {
-  return (
-    <div
-      className={cn(
-        "z-999 absolute top-1 right-1 filter backdrop-blur-lg backdrop-brightness-120",
-        "flex flex-row-reverse rounded text-on-background bg-background/80",
-      )}
-    >
-      <BasicPopover
-        trigger={<XIcon data-icon-type="remove" weight="duotone" className="grayscale size-6" />}
-        sideOffset={4}
-        side="right"
-      >
-        <button
-          type="button"
-          className="cursor-pointer"
-          onPointerDown={(e) => {
-            e.stopPropagation();
-            const itemId = e.currentTarget.dataset.itemId as string;
-            uiStoreApi.removeItem(itemId);
-          }}
-          data-item-id={meta.id}
-        >
-          confirm
-        </button>
-      </BasicPopover>
-
-      <button
-        type="button"
-        data-item-id={meta.id}
-        className={cn(allowReactGridDragClassName, "cursor-move p-1")}
-      >
-        <LayoutIcon data-icon-type="layout" weight="duotone" className="size-5" />
-      </button>
-
-      <button
-        type="button"
-        data-item-id={meta.id}
-        className="p-1 cursor-pointer"
-        onClick={() => {
-          // toggle item disabled and sync sub-uis
-          uiStoreApi.setUiMeta(meta.id, (draft) => (draft.disabled = !draft.disabled));
-          uiStoreApi.getSubUis(meta.id)?.forEach(({ meta: subMeta }) => {
-            uiStoreApi.setUiMeta(subMeta.id, (draft) => (draft.disabled = !draft.disabled));
-          });
-        }}
-      >
-        <PlayCircleIcon
-          data-icon-type="play"
-          weight="duotone"
-          className={cn("size-5", meta.disabled ? "text-gray-500" : "text-green-700")}
-        />
-      </button>
-    </div>
-  );
-}
