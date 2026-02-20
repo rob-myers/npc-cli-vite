5ebe9f31d70706354eceffb7267dc6e0eb1578ff
rob-myers <me.robmyers@gmail.com>
2026-02-20 23:56:18 +0000

    feat: starship symbol images: symbol-misc

--- scripts/src/bins/get-pngs.ts
+++ scripts/src/bins/get-pngs.ts
@@ -44,11 +44,11 @@ import {
  * pnpm get-pngs geomorph 'Geomorphs/100x100 Core' geomorph-core
  * pnpm get-pngs symbol Symbols/Bridge symbol-bridge
  * pnpm get-pngs small-craft 'Small Craft' symbol-small-craft
- *
  * pnpm get-pngs symbol 'Symbols/Lab' symbol-lab
+ * pnpm get-pngs symbol 'Symbols/Misc' symbol-misc
+ *
  * pnpm get-pngs symbol 'Symbols/Battery' symbol-battery
  * pnpm get-pngs symbol 'Symbols/Medical' symbol-medical
- * pnpm get-pngs symbol 'Symbols/Misc' symbol-misc
  * pnpm get-pngs symbol 'Symbols/Offices' symbol-offices
  * pnpm get-pngs symbol 'Symbols/Shop & Repair Area' symbol-shop-repair-area
  * pnpm get-pngs symbol Symbols/Fresher symbol-fresher
