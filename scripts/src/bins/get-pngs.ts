f42b3b9a6ad59a4307221850c2406bf2e175012f
rob-myers <me.robmyers@gmail.com>
2026-02-21 00:06:17 +0000

    feat: starship symbol images: symbol-medical

--- scripts/src/bins/get-pngs.ts
+++ scripts/src/bins/get-pngs.ts
@@ -49,8 +49,8 @@ import {
  * pnpm get-pngs symbol 'Symbols/Offices' symbol-offices
  * pnpm get-pngs symbol 'Symbols/Galley & Mess' symbol-galley-and-mess
  * pnpm get-pngs symbol 'Symbols/Battery' symbol-battery
- *
  * pnpm get-pngs symbol 'Symbols/Medical' symbol-medical
+ *
  * pnpm get-pngs symbol 'Symbols/Offices' symbol-offices
  * pnpm get-pngs symbol 'Symbols/Shop & Repair Area' symbol-shop-repair-area
  * pnpm get-pngs symbol Symbols/Fresher symbol-fresher
