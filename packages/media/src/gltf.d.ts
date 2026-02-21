0095cd2f318e70b7793c0e7ae3045c9ee4ecea2b
rob-myers <me.robmyers@gmail.com>
2026-02-21 15:23:22 +0000

    chore: will only import .gltf?url

--- packages/media/src/gltf.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "*.gltf" {
-  const content: string;
-  export default content;
-}
--- packages/media/src/index.ts
+++ packages/media/src/index.ts
@@ -1,8 +1,6 @@
 import testBlockBench5Gltf from "./blockbench/TestBlockbench5.gltf?url";
 import walkingRobotGuyGltf from "./blockbench/walking-robot-guy-blockbench-animation/source/model.gltf?url";
 
-import "./gltf.d";
-
 export const url = {
   testBlockBench5Gltf,
   walkingRobotGuyGltf,
