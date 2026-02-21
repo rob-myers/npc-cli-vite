1e2216a82393289b0901bede453144f9f9bbb8aa
rob-myers <me.robmyers@gmail.com>
2026-02-21 00:23:35 +0000

    feat: starship symbol images

--- scripts/src/bins/get-pngs.ts
+++ scripts/src/bins/get-pngs.ts
@@ -46,7 +46,7 @@ import {
  * pnpm get-pngs small-craft 'Small Craft' symbol-small-craft
  * pnpm get-pngs symbol 'Symbols/Lab' symbol-lab
  * pnpm get-pngs symbol 'Symbols/Misc' symbol-misc
- * pnpm get-pngs symbol 'Symbols/Offices' symbol-offices
+ * pnpm get-pngs symbol 'Symbols/Offices' symbol-office
  * pnpm get-pngs symbol 'Symbols/Galley & Mess' symbol-galley-and-mess
  * pnpm get-pngs symbol 'Symbols/Battery' symbol-battery
  * pnpm get-pngs symbol 'Symbols/Medical' symbol-medical
@@ -57,9 +57,9 @@ import {
  * pnpm get-pngs symbol 'Symbols/Fuel' symbol-fuel
  * pnpm get-pngs symbol 'Symbols/Lounge' symbol-lounge
  * pnpm get-pngs symbol 'Symbols/Low Berth' symbol-low-berth
- *
+ * pnpm get-pngs symbol "Symbols/Ship's Locker" symbol-ships-locker
  * pnpm get-pngs symbol 'Symbols/Shop & Repair Area' symbol-shop-repair-area
- * pnpm get-pngs symbol Symbols/Fresher symbol-fresher
+ * pnpm get-pngs symbol 'Symbols/Staterooms' symbol-stateroom
  * ```
  */
 
