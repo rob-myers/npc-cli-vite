1bc38c352654236a0b44cd7b160d16ded7ae56af
rob-myers <me.robmyers@gmail.com>
2026-02-21 00:04:06 +0000

    feat: starship symbol images: symbol-galley-and-mess

--- scripts/src/bins/get-pngs.ts
+++ scripts/src/bins/get-pngs.ts
@@ -47,6 +47,7 @@ import {
  * pnpm get-pngs symbol 'Symbols/Lab' symbol-lab
  * pnpm get-pngs symbol 'Symbols/Misc' symbol-misc
  * pnpm get-pngs symbol 'Symbols/Offices' symbol-offices
+ * pnpm get-pngs symbol 'Symbols/Galley & Mess' symbol-galley-and-mess
  *
  * pnpm get-pngs symbol 'Symbols/Battery' symbol-battery
  * pnpm get-pngs symbol 'Symbols/Medical' symbol-medical
