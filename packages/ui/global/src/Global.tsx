9a529ddcd154f3f999e3cc3f683d241bc1ba2f1b
rob-myers <me.robmyers@gmail.com>
2026-02-22 18:59:43 +0000

    feat: Global: play with grid for resize

--- packages/ui/global/src/Global.tsx
+++ packages/ui/global/src/Global.tsx
@@ -5,19 +5,25 @@ import { BasicPopover, cn } from "@npc-cli/util";
 export default function Global() {
   return (
     <div className="flex justify-center items-center h-full overflow-auto">
-      <div className="p-4 flex flex-wrap gap-2 justify-center">
+      <div className="p-4 grid grid-cols-2 w-48 gap-2">
         <button
           type="button"
           className={cn(
             uiClassName,
-            "cursor-pointer border rounded px-4 py-1 bg-button-background",
+            "cursor-pointer",
+            "overflow-auto border rounded",
+            "flex justify-center items-center bg-button-background",
           )}
           onPointerDown={themeApi.setOther}
         >
           {themeApi.getOther()}
         </button>
         <BasicPopover
-          triggerClassName={cn(uiClassName, "border rounded px-4 py-1 bg-button-background")}
+          triggerClassName={cn(
+            uiClassName,
+            "overflow-auto border rounded",
+            "flex justify-center items-center bg-button-background",
+          )}
           trigger={"reset"}
           side="bottom"
         >
