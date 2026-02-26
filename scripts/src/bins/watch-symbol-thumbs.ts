658f27fc412f9f28d12e534cffa802c22d93a742
rob-myers <me.robmyers@gmail.com>
2026-02-26 16:33:19 +0000

    feat: script watches public/symbol and executes on change

--- scripts/src/bins/watch-symbol-thumbs.ts
+++ scripts/src/bins/watch-symbol-thumbs.ts
@@ -1,10 +1,68 @@
 #!/usr/bin/env node --import=tsx
 
-import { ansi } from "@npc-cli/cli/shell/const";
+import { info } from "@npc-cli/util/legacy/generic";
+import nodemon, { type NodemonEventQuit } from "nodemon";
+import { PROJECT_ROOT } from "../const";
 
-console.log(`${ansi.Blue}Watching symbols...${ansi.Reset}`);
+// import { labelledSpawn } from "../starship-symbol/service";
 
-while (true) {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  throw new Error("Simulated crash");
+/** Is the script currently running? */
+let running = false;
+/** We pause to allow multiple changes to aggregate */
+const delayMs = 300;
+/** Absolute path to `Date.now()` */
+const changed = /** @type {Map<string, number>} */ (new Map());
+
+nodemon({
+  delay: 0.1,
+  ext: "json",
+  runOnChangeOnly: true,
+  script: "scripts/src/noop.js", // 🔔 must override default behaviour
+  cwd: PROJECT_ROOT,
+  watch: ["packages/app/public/symbol/*.json"],
+  exitCrash: true,
+})
+  .on("restart", onRestart)
+  .on("quit", onQuit);
+
+info("watching symbols...");
+
+async function onRestart(nodemonFiles = [] as string[]) {
+  nodemonFiles.forEach((file) => changed.set(file, Date.now()));
+
+  if (running) return;
+  running = true;
+
+  // pause to aggregate changes
+  await new Promise((resolve) => setTimeout(resolve, delayMs));
+
+  const startEpochMs = Date.now();
+  const changedFiles = Array.from(changed.keys());
+
+  // 🚧 generate a thumbnail for each changed file
+  // 🚧 also generate all initially
+  console.log({ changedFiles });
+  // Run the script
+  // await labelledSpawn(
+  //   "assets",
+  //   // 'sucrase-node',
+  //   "bun",
+  //   "scripts/assets",
+  //   `--changedFiles=${JSON.stringify(changedFiles)}`,
+  // );
+
+  const seconds = ((Date.now() - startEpochMs) / 1000).toFixed(2);
+  info(`took ${seconds}s`);
+
+  changed.forEach((epochMs, file) => epochMs <= startEpochMs && changed.delete(file));
+  running = false;
+  if (changed.size > 0) {
+    // something changed after we started the script
+    await onRestart();
+  }
+}
+
+function onQuit(_code?: NodemonEventQuit) {
+  // console.log('quit', code);
+  process.exit();
 }
