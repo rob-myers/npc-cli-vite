f3430cac52cc4614554d1a709c902be43ea77bcc
rob-myers <me.robmyers@gmail.com>
2026-03-11 17:03:34 +0000

    feat: createdAt -> modifiedAt + accurate change logging

--- scripts/src/service/process-symbol.ts
+++ scripts/src/service/process-symbol.ts
@@ -8,14 +8,16 @@ import {
   MapEditMapFileSpecifierSchema,
   MapEditSavedFileSchema,
   MapEditSymbolFileSpecifierSchema,
+  MapsManifestItemSchema,
   MapsManifestSchema,
   migrateMapEditSavedFile,
+  SymbolsManifestItemSchema,
   SymbolsManifestSchema,
   traverseNodesAsync,
 } from "@npc-cli/ui__map-edit/map-node-api";
 import { Mat, Rect } from "@npc-cli/util/geom";
 import { jsonParser } from "@npc-cli/util/json-parser";
-import { error, warn } from "@npc-cli/util/legacy/generic";
+import { deepClone, error, info, warn } from "@npc-cli/util/legacy/generic";
 import { Canvas, loadImage } from "skia-canvas";
 import z from "zod";
 import { PROJECT_ROOT } from "../const.ts";
@@ -113,19 +115,23 @@ async function ensureManifests(type: "symbol" | "map", opts: ProcessFileOpts) {
     type === "symbol"
       ? ({
           manifest: SymbolsManifestSchema,
+          entryManifest: SymbolsManifestItemSchema,
           fileSpecifier: MapEditSymbolFileSpecifierSchema,
         } as const)
       : ({
           manifest: MapsManifestSchema,
+          entryManifest: MapsManifestItemSchema,
           fileSpecifier: MapEditMapFileSpecifierSchema,
         } as const);
 
-  const createdAt = new Date().toISOString();
   const directory = path.resolve(PROJECT_ROOT, `packages/app/public/${type}`);
   const manifestPath = path.resolve(directory, "manifest.json");
-  const nextManifest = jsonParser
+
+  const prevManifest = jsonParser
     .pipe(schema.manifest)
-    .safeParse(await fs.promises.readFile(manifestPath, "utf-8").catch(warn)).data ?? { createdAt, byKey: {} };
+    .safeParse(await fs.promises.readFile(manifestPath, "utf-8").catch(warn)).data;
+  // mutate existing if possible
+  const byKey = deepClone(prevManifest?.byKey ?? {});
 
   // Must exclude manifest.json
   const itemFilePaths = fs.globSync(path.resolve(directory, "*.json")).filter((filePath) => filePath !== manifestPath);
@@ -134,7 +140,7 @@ async function ensureManifests(type: "symbol" | "map", opts: ProcessFileOpts) {
 
   for (const filePath of itemFilePaths) {
     const filename = path.basename(filePath);
-    const key = path.basename(filename, ".json") as keyof typeof nextManifest.byKey;
+    const key = path.basename(filename, ".json") as keyof typeof byKey;
     const fileSpecifierResult = schema.fileSpecifier.safeParse({ type, key, filename });
 
     if (!fileSpecifierResult.success) {
@@ -142,25 +148,26 @@ async function ensureManifests(type: "symbol" | "map", opts: ProcessFileOpts) {
       continue;
     }
 
-    if (!nextManifest.byKey[key] || changedFilenames.includes(filename)) {
+    if (!byKey[key] || changedFilenames.includes(filename)) {
       const savedFileResult = jsonParser.pipe(MapEditSavedFileSchema).safeParse(readFileSync(filePath, "utf-8"));
       if (savedFileResult.success) {
-        nextManifest.byKey[key] = {
+        // must re-parse to ensure key-order for JSON.stringify equality-test
+        byKey[key] = schema.entryManifest.parse({
           ...fileSpecifierResult.data,
           thumbnailFilename: `${path.basename(filename, ".json")}.thumbnail.png`,
           width: savedFileResult.data.width,
           height: savedFileResult.data.height,
           bounds: savedFileResult.data.bounds,
-        };
+        });
       } else {
         error(`Failed to parse existing ${type}: ${filename}`, z.prettifyError(savedFileResult.error));
       }
     }
 
     if (opts.removedFiles?.some((x) => x.filename === filename)) {
-      const entry = nextManifest.byKey[key];
+      const entry = byKey[key];
       if (entry) {
-        delete nextManifest.byKey[key];
+        delete byKey[key];
         await fs.promises.unlink(path.resolve(directory, entry.filename)).catch(warn);
         await fs.promises.unlink(path.resolve(directory, entry.thumbnailFilename)).catch(warn);
       } else {
@@ -169,5 +176,12 @@ async function ensureManifests(type: "symbol" | "map", opts: ProcessFileOpts) {
     }
   }
 
+  if (JSON.stringify(byKey) === JSON.stringify(prevManifest?.byKey)) {
+    info(`${path.basename(import.meta.filename)}: ${type}: no changes detected`);
+    return;
+  }
+
+  info(`${path.basename(import.meta.filename)}: ${type}: changes detected`);
+  const nextManifest: typeof prevManifest = { modifiedAt: new Date().toISOString(), byKey };
   fs.writeFileSync(manifestPath, JSON.stringify(nextManifest, null, 2));
 }
