0372e9482be89ba857fdcc5047af60bcd0cf24c4
rob-myers <me.robmyers@gmail.com>
2026-03-12 12:18:12 +0000

    chore: clean map-node-api

--- packages/ui/map-edit/src/map-node-api.ts
+++ packages/ui/map-edit/src/map-node-api.ts
@@ -2,8 +2,245 @@ import { StarShipSymbolImageKeySchema, type StarshipSymbolImageKey } from "@npc-
 import { ExhaustiveError } from "@npc-cli/util/exhaustive-error";
 import { Mat, Rect } from "@npc-cli/util/geom";
 import { keys, tryLocalStorageGetParsed, warn } from "@npc-cli/util/legacy/generic";
-import z from "zod"; /** Find node and its parent */
+import z from "zod";
 
+//#region schemas
+
+const PointSchema = z.object({
+  x: z.number(),
+  y: z.number(),
+});
+const BaseRectSchema = z.object({
+  width: z.number(),
+  height: z.number(),
+});
+const RectSchema = BaseRectSchema.extend(PointSchema.shape);
+const TransformSchema = z.object({
+  a: z.number(),
+  b: z.number(),
+  c: z.number(),
+  d: z.number(),
+  e: z.number(),
+  f: z.number(),
+});
+
+const BaseNodeSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  locked: z.boolean(),
+  visible: z.boolean(),
+  transform: TransformSchema,
+});
+export type BaseMapNode = z.infer<typeof BaseNodeSchema>;
+
+export const MapNodeSchema = z.union([
+  BaseNodeSchema.extend({
+    type: z.literal("group"),
+    get children() {
+      return z.array(MapNodeSchema);
+    },
+  }),
+  BaseNodeSchema.extend({
+    type: z.literal("rect"),
+    baseRect: z.object({ width: z.number(), height: z.number() }),
+    cssTransform: z.string(),
+  }),
+  BaseNodeSchema.extend({
+    type: z.literal("image"),
+    srcKey: StarShipSymbolImageKeySchema.nullable(),
+    baseRect: z.object({ width: z.number(), height: z.number() }),
+    offset: PointSchema,
+    cssTransform: z.string(),
+  }),
+  BaseNodeSchema.extend({
+    type: z.literal("symbol"),
+    srcKey: StarShipSymbolImageKeySchema.nullable(),
+    baseRect: z.object({ width: z.number(), height: z.number() }),
+    offset: PointSchema,
+    cssTransform: z.string(),
+  }),
+  BaseNodeSchema.extend({
+    type: z.literal("path"),
+    d: z.string(),
+    baseRect: z.object({ width: z.number(), height: z.number() }),
+    cssTransform: z.string(),
+  }),
+]);
+
+export type MapNode = z.infer<typeof MapNodeSchema>;
+export type MapNodeType = MapNode["type"];
+export type RectMapNode = Pretty<Extract<MapNode, { type: "rect" }>>;
+export type GroupMapNode = Pretty<Extract<MapNode, { type: "group" }>>;
+export type TransformableMapNode = Extract<MapNode, { type: "rect" | "image" | "symbol" | "path" }>;
+export type BaseRect = { width: number; height: number };
+export type Transform = z.infer<typeof TransformSchema>;
+export type MapNodeByType<T extends MapNodeType> = Pretty<Extract<MapNode, { type: T }>>;
+export type MapNodeMap = { [T in MapNodeType]: MapNodeByType<T> };
+
+export const SymbolKeySchema = StarShipSymbolImageKeySchema;
+export const SymbolJsonFilenameSchema = z.templateLiteral([SymbolKeySchema, ".json"]);
+/** AKA `StarShipSymbolImageKey` */
+export type SymbolKey = z.infer<typeof StarShipSymbolImageKeySchema>;
+
+export const MapKeySchema = z.string();
+export const MapJsonFilenameSchema = MapKeySchema.endsWith(".json");
+
+export const MapEditSymbolFileSpecifierSchema = z.object({
+  type: z.literal("symbol"),
+  filename: SymbolJsonFilenameSchema,
+  key: StarShipSymbolImageKeySchema,
+});
+export const MapEditMapFileSpecifierSchema = z.object({
+  type: z.literal("map"),
+  filename: MapJsonFilenameSchema,
+  key: z.string(),
+});
+export const MapEditFileSpecifierSchema = z.union([MapEditSymbolFileSpecifierSchema, MapEditMapFileSpecifierSchema]);
+export type MapEditFileSpecifier = z.infer<typeof MapEditFileSpecifierSchema>;
+
+const MapEditSavedBaseSchema = z.object({
+  filename: z.string(),
+  width: z.number(),
+  height: z.number(),
+  nodes: z.array(MapNodeSchema),
+  bounds: RectSchema,
+});
+
+export const MapEditSavedSymbolSchema = MapEditSavedBaseSchema.extend(MapEditSymbolFileSpecifierSchema.shape);
+export const MapEditSavedMapSchema = MapEditSavedBaseSchema.extend(MapEditMapFileSpecifierSchema.shape);
+export const MapEditSavedFileSchema = z.union([MapEditSavedSymbolSchema, MapEditSavedMapSchema]);
+
+export type MapEditSavedFile = z.infer<typeof MapEditSavedFileSchema>;
+export type UiIdToCurrentFileSpecifer = Record<string, MapEditFileSpecifier>;
+
+const BaseManifestItemSchema = z.object({
+  filename: z.string(),
+  thumbnailFilename: z.string(),
+  width: z.number(),
+  height: z.number(),
+  bounds: RectSchema,
+});
+
+export const SymbolsManifestItemSchema = BaseManifestItemSchema.extend(MapEditSymbolFileSpecifierSchema.shape);
+
+export const SymbolsManifestSchema = z.object({
+  modifiedAt: z.string(),
+  byKey: z.partialRecord(StarShipSymbolImageKeySchema, SymbolsManifestItemSchema),
+});
+
+export const MapsManifestItemSchema = BaseManifestItemSchema.extend(MapEditMapFileSpecifierSchema.shape);
+
+export const MapsManifestSchema = z.object({
+  modifiedAt: z.string(),
+  byKey: z.record(z.string(), MapsManifestItemSchema),
+});
+
+export const PathManifestEntrySchema = z.object({
+  key: z.string(),
+  filename: z.string(),
+  pathCount: z.number(),
+  width: z.number(),
+  height: z.number(),
+});
+
+export const PathManifestSchema = z.object({
+  modifiedAt: z.string(),
+  byKey: z.record(z.string(), PathManifestEntrySchema),
+});
+
+export type SymbolsManifest = z.infer<typeof SymbolsManifestSchema>;
+export type MapsManifest = z.infer<typeof MapsManifestSchema>;
+export type PathManifest = z.infer<typeof PathManifestSchema>;
+
+//#endregion
+
+//#region constants
+
+const defaultBaseRect: BaseRect = { width: 60, height: 60 };
+const defaultPoint: Geom.VectJson = { x: 0, y: 0 };
+const idTransform: Transform = { a: 1, b: 0, c: 0, d: 1, e: 0, f: 0 };
+
+const mockBaseNode: BaseMapNode = {
+  id: "mock-id",
+  name: "New Node",
+  locked: false,
+  visible: true,
+  transform: { ...idTransform },
+};
+
+export const templateNodeByKey = {
+  group: { ...mockBaseNode, type: "group", children: [] as MapNode[] },
+  image: {
+    ...mockBaseNode,
+    type: "image",
+    srcKey: null,
+    baseRect: defaultBaseRect,
+    offset: defaultPoint,
+    cssTransform: "matrix(1, 0, 0, 1, 0, 0)",
+  },
+  rect: {
+    ...mockBaseNode,
+    type: "rect",
+    baseRect: defaultBaseRect,
+    cssTransform: "translate(0px, 0px) scale(1)",
+  },
+  symbol: {
+    ...mockBaseNode,
+    type: "symbol",
+    srcKey: null,
+    baseRect: defaultBaseRect,
+    offset: defaultPoint,
+    cssTransform: "matrix(1, 0, 0, 1, 0, 0)",
+  },
+  path: {
+    ...mockBaseNode,
+    type: "path",
+    d: "",
+    baseRect: defaultBaseRect,
+    cssTransform: "matrix(1, 0, 0, 1, 0, 0)",
+  },
+} satisfies Record<MapNodeType, MapNode>;
+
+export const mapNodeTypes = keys({
+  group: true,
+  image: true,
+  path: true,
+  rect: true,
+  symbol: true,
+} satisfies Record<MapNodeType, true>);
+
+export const baseSvgSize = 600;
+
+export const labelledImageOffsetValue = {
+  zero: 0,
+  centerExtra003: -0.3,
+  halfLineWidth: -0.7,
+  eastAlignBed004: -3.7,
+  centerXConsole051: 1.3,
+  centerYStateRoom012: 2,
+  centerXExtra004: -3,
+} as const;
+
+export const imageOffsetValues = Object.values(labelledImageOffsetValue)
+  .flatMap((x) => (x === 0 ? 0 : [Math.abs(x), -Math.abs(x)]))
+  .sort();
+
+export const ALLOWED_MAP_EDIT_FOLDERS = ["symbol", "map"] as const;
+
+export const LOCAL_STORAGE_PREFIX = "map-edit:";
+export const LOCAL_STORAGE_UI_ID_TO_FILE_SPECIFIER = "map-edit-to-current-file";
+
+export const defaultSymbolKey: StarshipSymbolImageKey = "stateroom--012--2x2";
+
+export const devMessageFromServer = {
+  recomputedPathManifest: "map-edit:recompute-path-manifest",
+} as const;
+
+//#endregion
+
+//#region node apis
+
+/** Find node and its parent */
 export function findNode(
   /** Either top-level nodes or `group.childrem` */
   parentArray: MapNode[],
@@ -158,185 +395,6 @@ export async function traverseNodesAsync(list: MapNode[], act: (el: MapNode) => 
   }
 }
 
-const defaultBaseRect: BaseRect = { width: 60, height: 60 };
-const defaultPoint: Geom.VectJson = { x: 0, y: 0 };
-const idTransform: Transform = { a: 1, b: 0, c: 0, d: 1, e: 0, f: 0 };
-
-const mockBaseNode: BaseMapNode = {
-  id: "mock-id",
-  name: "New Node",
-  locked: false,
-  visible: true,
-  transform: { ...idTransform },
-};
-
-export const templateNodeByKey = {
-  group: { ...mockBaseNode, type: "group", children: [] as MapNode[] },
-  image: {
-    ...mockBaseNode,
-    type: "image",
-    srcKey: null,
-    baseRect: defaultBaseRect,
-    offset: defaultPoint,
-    cssTransform: "matrix(1, 0, 0, 1, 0, 0)",
-  },
-  rect: {
-    ...mockBaseNode,
-    type: "rect",
-    baseRect: defaultBaseRect,
-    cssTransform: "translate(0px, 0px) scale(1)",
-  },
-  symbol: {
-    ...mockBaseNode,
-    type: "symbol",
-    srcKey: null,
-    baseRect: defaultBaseRect,
-    offset: defaultPoint,
-    cssTransform: "matrix(1, 0, 0, 1, 0, 0)",
-  },
-  path: {
-    ...mockBaseNode,
-    type: "path",
-    d: "",
-    baseRect: defaultBaseRect,
-    cssTransform: "matrix(1, 0, 0, 1, 0, 0)",
-  },
-} satisfies Record<MapNodeType, MapNode>;
-
-const PointSchema = z.object({
-  x: z.number(),
-  y: z.number(),
-});
-const BaseRectSchema = z.object({
-  width: z.number(),
-  height: z.number(),
-});
-const RectSchema = BaseRectSchema.extend(PointSchema.shape);
-const TransformSchema = z.object({
-  a: z.number(),
-  b: z.number(),
-  c: z.number(),
-  d: z.number(),
-  e: z.number(),
-  f: z.number(),
-});
-
-const BaseNodeSchema = z.object({
-  id: z.string(),
-  name: z.string(),
-  locked: z.boolean(),
-  visible: z.boolean(),
-  transform: TransformSchema,
-});
-export type BaseMapNode = z.infer<typeof BaseNodeSchema>;
-
-export const MapNodeSchema = z.union([
-  BaseNodeSchema.extend({
-    type: z.literal("group"),
-    get children() {
-      return z.array(MapNodeSchema);
-    },
-  }),
-  BaseNodeSchema.extend({
-    type: z.literal("rect"),
-    baseRect: z.object({ width: z.number(), height: z.number() }),
-    cssTransform: z.string(),
-  }),
-  BaseNodeSchema.extend({
-    type: z.literal("image"),
-    srcKey: StarShipSymbolImageKeySchema.nullable(),
-    baseRect: z.object({ width: z.number(), height: z.number() }),
-    offset: PointSchema,
-    cssTransform: z.string(),
-  }),
-  BaseNodeSchema.extend({
-    type: z.literal("symbol"),
-    srcKey: StarShipSymbolImageKeySchema.nullable(),
-    baseRect: z.object({ width: z.number(), height: z.number() }),
-    offset: PointSchema,
-    cssTransform: z.string(),
-  }),
-  BaseNodeSchema.extend({
-    type: z.literal("path"),
-    d: z.string(),
-    baseRect: z.object({ width: z.number(), height: z.number() }),
-    cssTransform: z.string(),
-  }),
-]);
-
-export type MapNode = z.infer<typeof MapNodeSchema>;
-export type MapNodeType = MapNode["type"];
-export type RectMapNode = Pretty<Extract<MapNode, { type: "rect" }>>;
-export type GroupMapNode = Pretty<Extract<MapNode, { type: "group" }>>;
-export type TransformableMapNode = Extract<MapNode, { type: "rect" | "image" | "symbol" | "path" }>;
-
-export const mapNodeTypes = keys({
-  group: true,
-  image: true,
-  path: true,
-  rect: true,
-  symbol: true,
-} satisfies Record<MapNodeType, true>);
-
-export type BaseRect = { width: number; height: number };
-export type Transform = z.infer<typeof TransformSchema>;
-
-export type MapNodeByType<T extends MapNodeType> = Pretty<Extract<MapNode, { type: T }>>;
-export type MapNodeMap = { [T in MapNodeType]: MapNodeByType<T> };
-
-export const baseSvgSize = 600;
-
-export const labelledImageOffsetValue = {
-  zero: 0,
-  centerExtra003: -0.3,
-  halfLineWidth: -0.7,
-  eastAlignBed004: -3.7,
-  centerXConsole051: 1.3,
-  centerYStateRoom012: 2,
-  centerXExtra004: -3,
-} as const;
-
-export const imageOffsetValues = Object.values(labelledImageOffsetValue)
-  .flatMap((x) => (x === 0 ? 0 : [Math.abs(x), -Math.abs(x)]))
-  .sort();
-
-export const ALLOWED_MAP_EDIT_FOLDERS = ["symbol", "map"] as const;
-
-export const SymbolKeySchema = StarShipSymbolImageKeySchema;
-export const SymbolJsonFilenameSchema = z.templateLiteral([SymbolKeySchema, ".json"]);
-/** AKA `StarShipSymbolImageKey` */
-export type SymbolKey = z.infer<typeof StarShipSymbolImageKeySchema>;
-
-export const MapKeySchema = z.string();
-export const MapJsonFilenameSchema = MapKeySchema.endsWith(".json");
-
-export const MapEditSymbolFileSpecifierSchema = z.object({
-  type: z.literal("symbol"),
-  filename: SymbolJsonFilenameSchema,
-  key: StarShipSymbolImageKeySchema,
-});
-export const MapEditMapFileSpecifierSchema = z.object({
-  type: z.literal("map"),
-  filename: MapJsonFilenameSchema,
-  key: z.string(),
-});
-export const MapEditFileSpecifierSchema = z.union([MapEditSymbolFileSpecifierSchema, MapEditMapFileSpecifierSchema]);
-export type MapEditFileSpecifier = z.infer<typeof MapEditFileSpecifierSchema>;
-
-const MapEditSavedBaseSchema = z.object({
-  filename: z.string(),
-  width: z.number(),
-  height: z.number(),
-  nodes: z.array(MapNodeSchema),
-  bounds: RectSchema,
-});
-
-export const MapEditSavedSymbolSchema = MapEditSavedBaseSchema.extend(MapEditSymbolFileSpecifierSchema.shape);
-export const MapEditSavedMapSchema = MapEditSavedBaseSchema.extend(MapEditMapFileSpecifierSchema.shape);
-export const MapEditSavedFileSchema = z.union([MapEditSavedSymbolSchema, MapEditSavedMapSchema]);
-
-export type MapEditSavedFile = z.infer<typeof MapEditSavedFileSchema>;
-
 export function isSavableFileType(type: string): type is MapEditSavedFile["type"] {
   return ALLOWED_MAP_EDIT_FOLDERS.includes(type as MapEditSavedFile["type"]);
 }
@@ -380,8 +438,6 @@ export function areFileSpecifiersEqual(a: MapEditFileSpecifier, b: MapEditFileSp
   return a.type === b.type && a.filename === b.filename;
 }
 
-export type UiIdToCurrentFileSpecifer = Record<string, MapEditFileSpecifier>;
-
 export function extendCurrentFileSpecifierMapping(
   uiId: string,
   fileSpecifier: MapEditFileSpecifier,
@@ -391,50 +447,6 @@ export function extendCurrentFileSpecifierMapping(
     [uiId]: fileSpecifier,
   };
 }
-
-export const LOCAL_STORAGE_PREFIX = "map-edit:";
-export const LOCAL_STORAGE_UI_ID_TO_FILE_SPECIFIER = "map-edit-to-current-file";
-
-const BaseManifestItemSchema = z.object({
-  filename: z.string(),
-  thumbnailFilename: z.string(),
-  width: z.number(),
-  height: z.number(),
-  bounds: RectSchema,
-});
-
-export const SymbolsManifestItemSchema = BaseManifestItemSchema.extend(MapEditSymbolFileSpecifierSchema.shape);
-
-export const SymbolsManifestSchema = z.object({
-  modifiedAt: z.string(),
-  byKey: z.partialRecord(StarShipSymbolImageKeySchema, SymbolsManifestItemSchema),
-});
-
-export const MapsManifestItemSchema = BaseManifestItemSchema.extend(MapEditMapFileSpecifierSchema.shape);
-
-export const MapsManifestSchema = z.object({
-  modifiedAt: z.string(),
-  byKey: z.record(z.string(), MapsManifestItemSchema),
-});
-
-export const PathManifestEntrySchema = z.object({
-  key: z.string(),
-  filename: z.string(),
-  pathCount: z.number(),
-  width: z.number(),
-  height: z.number(),
-});
-
-export const PathManifestSchema = z.object({
-  modifiedAt: z.string(),
-  byKey: z.record(z.string(), PathManifestEntrySchema),
-});
-
-export type SymbolsManifest = z.infer<typeof SymbolsManifestSchema>;
-export type MapsManifest = z.infer<typeof MapsManifestSchema>;
-export type PathManifest = z.infer<typeof PathManifestSchema>;
-
-export const defaultSymbolKey: StarshipSymbolImageKey = "stateroom--012--2x2";
 
 /**
  * ⚠️ Used to fix saved files as we migrate schemas.
@@ -447,6 +459,4 @@ export function migrateMapEditSavedFile(savedFile: MapEditSavedFile): MapEditSav
   return savedFile;
 }
 
-export const devMessageFromServer = {
-  recomputedPathManifest: "map-edit:recompute-path-manifest",
-} as const;
+//#endregion
