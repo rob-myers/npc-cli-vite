d61e8512ae9902371a273983eff64413b58f9755
rob-myers <me.robmyers@gmail.com>
2026-03-12 16:40:18 +0000

    chore: clean

--- packages/media/src/index.ts
+++ packages/media/src/index.ts
@@ -1,9 +1,5 @@
-import templateGltf from "./blockbench/fiverr/template.gltf?url";
-import testBlockBench5Gltf from "./blockbench/TestBlockbench5.gltf?url";
-import walkingRobotGuyGltf from "./blockbench/walking-robot-guy-blockbench-animation/source/model.gltf?url";
+import templateGltf from "./blockbench/template.gltf?url";
 
 export const url = {
-  testBlockBench5Gltf,
-  walkingRobotGuyGltf,
   templateGltf,
 };
