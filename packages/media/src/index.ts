9e5989c7d98176ed6c7643a17736a3a70c9dbf80
rob-myers <me.robmyers@gmail.com>
2026-03-12 13:20:52 +0000

    feat: can see template.gltf

--- packages/media/src/index.ts
+++ packages/media/src/index.ts
@@ -1,7 +1,9 @@
+import templateGltf from "./blockbench/fiverr/template.gltf?url";
 import testBlockBench5Gltf from "./blockbench/TestBlockbench5.gltf?url";
 import walkingRobotGuyGltf from "./blockbench/walking-robot-guy-blockbench-animation/source/model.gltf?url";
 
 export const url = {
   testBlockBench5Gltf,
   walkingRobotGuyGltf,
+  templateGltf,
 };
---
+++ packages/ui/world/src/demo.tsx
@@ -0,0 +1,74 @@
+import { url } from "@npc-cli/media";
+import { useAnimations, useGLTF } from "@react-three/drei";
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
+
+export function TemplateGltfDemo() {
+  const groupRef = useRef<THREE.Group>(null);
+  const gltf = useGLTF(url.templateGltf);
+  const { actions } = useAnimations(gltf.animations, groupRef);
+  console.log({ gltf, actions });
+
+  useEffect(() => {
+    actions["walk"]?.reset().fadeIn(0.5).play();
+    return () => void actions["walk"]?.fadeOut(0.5);
+  }, [actions]);
+
+  return (
+    <group ref={groupRef}>
+      <primitive object={gltf.scene} />
+    </group>
+  );
+}
+
+export function WalkingRobotGuyGltfOnlyDemo() {
+  const groupRef = useRef<THREE.Group>(null);
+  const gltf = useGLTF(url.walkingRobotGuyGltf);
+  const { actions } = useAnimations(gltf.animations, groupRef);
+  console.log({ gltf, actions });
+
+  useEffect(() => {
+    actions["walk_animation"]?.reset().fadeIn(0.5).play();
+    return () => void actions["walk_animation"]?.fadeOut(0.5);
+  }, [actions]);
+
+  return (
+    <group ref={groupRef}>
+      <primitive object={gltf.scene} />
+    </group>
+  );
+}
+
+export function SkinnedMeshWithAnimationDemo() {
+  const groupRef = useRef<THREE.Group>(null);
+
+  // Blockbench: Export Groups as Armature
+  const gltf = useGLTF(url.testBlockBench5Gltf);
+  const { actions } = useAnimations(gltf.animations, groupRef);
+
+  useEffect(() => {
+    actions["dummy-animation"]?.reset().fadeIn(0.5).play();
+    return () => void actions["dummy-animation"]?.fadeOut(0.5);
+  }, [actions]);
+
+  const mesh = gltf.nodes["hips"] as THREE.SkinnedMesh;
+  const bones = Object.values(gltf.nodes).filter((n) => n instanceof THREE.Bone);
+  console.log({ gltf, actions, bones });
+
+  return (
+    <group ref={groupRef}>
+      <skinnedMesh
+        geometry={mesh.geometry}
+        // position={mesh.position}
+        skeleton={mesh.skeleton}
+        userData={mesh.userData}
+        material={mesh.material}
+      >
+        <primitive
+          object={bones[0]} // hips_1
+        />
+      </skinnedMesh>
+    </group>
+  );
+}
+0;
