f4f6d19c958c9464b815f5db5433081e39a60f8e
rob-myers <me.robmyers@gmail.com>
2026-03-13 11:04:51 +0000

    chore: clean gen-assets-json

--- packages/util/src/service/json-parser.ts
+++ packages/util/src/service/json-parser.ts
@@ -1,4 +1,5 @@
 import z from "zod";
+import { warn } from "../legacy/generic";
 
 /**
  * Compositional JSON parser for usage with other zod schemas.
@@ -7,7 +8,7 @@ export const jsonParser = z.string().transform((str, _ctx) => {
   try {
     return JSON.parse(str);
   } catch (_e) {
-    console.warn("Invalid JSON string:", str);
+    warn(`jsonParser: invalid JSON string: "${str}"`);
     return z.NEVER;
   }
 });
--- scripts/src/bins/gen-assets-json.tsx
+++ scripts/src/bins/gen-assets-json.tsx
@@ -4,40 +4,41 @@
  * 🚧 WIP
  * - `parsed` -- symbol key to geometry/metadata
  * - `stratified` -- stratified directed graph
- * - `flattened` -- symbol key to recursively unwound symbols (includes hull symbols)
+ * - `flattened` -- symbol key to unwound symbols (includes hull symbols)
  *
  * USAGE:
  * ```sh
  * pnpm gen-assets-json
- * pnpm gen-assets-json --changedFiles='["packages/app/public/symbol/console--051--0.4x0.6.json"]'
+ * pnpm gen-assets-json --changedFiles='["/path/to/.../public/symbol/console--051--0.4x0.6.json"]'
  *
  * # see also
  * pnpm watch-symbols
  * ```
- *
  */
 import fs from "node:fs";
 import path from "node:path";
 import { parseArgs } from "node:util";
 import { MapEditSavedFileSchema, SymbolJsonFilenameSchema } from "@npc-cli/ui__map-edit/map-node-api";
 import { jsonParser } from "@npc-cli/util/json-parser";
-import { error, info, safeJsonCompact, safeJsonParse } from "@npc-cli/util/legacy/generic";
+import { error, info, safeJsonCompact } from "@npc-cli/util/legacy/generic";
 import z from "zod";
+import { PROJECT_ROOT } from "../const";
 
 const opts = parseArgs({
   args: process.argv.slice(2),
   options: { changedFiles: { type: "string" } },
 });
 
+const inputChangedFiles = jsonParser.pipe(z.array(z.string())).safeParse(opts.values.changedFiles)?.data;
+if (opts.values.changedFiles !== undefined && !inputChangedFiles) {
+  error("When present --changedFiles must be a JSON array of strings.");
+  process.exit(1);
+}
+
 /** Paths .../packages/app/public/symbol/{symbolKey}.json */
 const changedFiles = (
-  (safeJsonParse(opts.values.changedFiles || "null") as string[] | null) ??
-  fs.globSync("packages/app/public/symbol/*.json")
+  inputChangedFiles ?? fs.globSync(path.resolve(PROJECT_ROOT, "packages/app/public/symbol/*.json"))
 ).filter((filePath) => SymbolJsonFilenameSchema.safeParse(path.basename(filePath)).success);
-
-if (!Array.isArray(changedFiles) || !changedFiles.every((file) => typeof file === "string")) {
-  throw new Error("If present --changedFiles must be a JSON array of strings.");
-}
 
 info(`[gen-assets-json]`, `changedFiles: ${safeJsonCompact(changedFiles)}`);
 
