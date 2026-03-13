39369caed8bf753b0bca20e263dca3c50618eebc
rob-myers <me.robmyers@gmail.com>
2026-03-13 09:50:01 +0000

    feat: Floor has checkerbox material

--- packages/ui/world/src/Floor.tsx
+++ packages/ui/world/src/Floor.tsx
@@ -1,11 +1,20 @@
 import { Box } from "@react-three/drei";
+import { useMemo } from "react";
+import { checker, color, float, positionLocal } from "three/tsl";
+import * as THREE from "three/webgpu";
 
 export default function Floor() {
+  const material = useMemo(() => {
+    const mat = new THREE.MeshBasicNodeMaterial();
+    const uv = positionLocal.xz.mul(float(16));
+    const check = checker(uv);
+    mat.colorNode = check.mix(color(0x222222), color(0xcccccc));
+    return mat;
+  }, []);
+
   return (
     <group>
-      <Box args={[1, 1, 1, 10, 1, 10]} position={[0, 0, 0]} scale={[100, 0.001, 100]}>
-        <meshBasicMaterial wireframe />
-      </Box>
+      <Box args={[1, 1, 1, 10, 1, 10]} position={[0, 0, 0]} scale={[100, 0.001, 100]} material={material} />
     </group>
   );
 }
--- packages/ui/world/src/World.tsx
+++ packages/ui/world/src/World.tsx
@@ -18,7 +18,7 @@ export default function World(props: { meta: WorldUiMeta }) {
   return (
     <WorldContext.Provider value={state}>
       <WorldView className={uiClassName}>
-        <ambientLight intensity={0.75} color="#ffffff" />
+        <ambientLight intensity={0.85} color="#ffffff" />
         <Floor />
         <Suspense>
           <NPCs />
