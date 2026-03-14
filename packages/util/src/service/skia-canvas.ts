5aa6462eff3a9e86a27fc7c8172e724c6772ff04
rob-myers <me.robmyers@gmail.com>
2026-03-14 14:05:34 +0000

    feat: util provides service/skia-canvas for drawing polygons

---
+++ packages/util/src/service/skia-canvas.ts
@@ -0,0 +1,40 @@
+import type { CanvasRenderingContext2D } from "skia-canvas";
+
+export function drawPolygons(
+  ct: CanvasRenderingContext2D,
+  polys: Geom.Poly | Geom.Poly[],
+  {
+    clip,
+    fillStyle,
+    strokeStyle,
+    lineWidth,
+  }: { clip?: boolean; fillStyle?: string | null; strokeStyle?: string | null; lineWidth?: number | null } = {},
+) {
+  polys = Array.isArray(polys) ? polys : [polys];
+  ct.fillStyle = fillStyle ?? ct.fillStyle;
+  ct.strokeStyle = strokeStyle ?? ct.strokeStyle;
+  ct.lineWidth = lineWidth ?? ct.lineWidth;
+  for (const poly of polys) {
+    ct.beginPath();
+    fillRing(ct, poly.outline, false);
+    for (const hole of poly.holes) {
+      fillRing(ct, hole, false);
+    }
+    ct.closePath();
+    if (strokeStyle !== null) {
+      ct.stroke();
+    }
+    if (fillStyle !== null) {
+      clip === true ? ct.clip() : ct.fill();
+    }
+  }
+}
+
+export function fillRing(ct: CanvasRenderingContext2D, ring: Geom.VectJson[], fill = true) {
+  if (ring.length) {
+    ct.moveTo(ring[0].x, ring[0].y);
+    ring.forEach((p) => ct.lineTo(p.x, p.y));
+    fill && ct.fill();
+    ct.closePath();
+  }
+}
