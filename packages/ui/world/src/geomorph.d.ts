927d6c6e584985585ae418086fccb7128bb105d2
rob-myers <me.robmyers@gmail.com>
2026-03-21 19:14:08 +0000

    feat: migrate decor types to schemas

--- packages/ui/world/src/geomorph.d.ts
+++ packages/ui/world/src/geomorph.d.ts
@@ -25,117 +25,26 @@ declare namespace Geomorph {
     roomId: number;
   }
 
+  /** `g${gmId}r${roomId}` */
+  type GmRoomKey = `g${number}r${number}`;
+
+  /** `g${gmId}d${doorId}` */
+  type GmDoorKey = `g${number}d${number}`;
+
   //#region decor
 
-  /**
-   * The actual decor instances in `<Decor>`.
-   * - They are serializable.
-   * - They're also used to represent un-instantiated layout decor in geomorphs.json.
-   */
-  type Decor = DecorCircle | DecorCuboid | DecorPoint | DecorQuad | DecorDecal | DecorRect;
+  type Decor = import("@npc-cli/ui__map-edit/map-node-api").Decor;
+  type DecorDef = import("@npc-cli/ui__map-edit/map-node-api").DecorDef;
 
-  /** Used during runtime creation. */
-  type DecorDef = DecorCircleDef | DecorCuboidDef | DecorPointDef | DecorQuadDef | DecorRectDef;
-
-  interface DecorCircle extends BaseDecor, Geom.Circle {
-    type: "circle";
-  }
-  interface DecorCircleDef extends BaseDecorDef, Geom.Circle {
-    type: "circle";
-  }
-
-  /**
-   * Vertices `center.xyz ± extent.xyz` rotated about `center` by `angle`.
-   */
-  interface DecorCuboid extends BaseDecor {
-    type: "cuboid";
-    center: import("three").Vector3Like;
-    transform: Geom.SixTuple;
-  }
-  interface DecorCuboidDef extends BaseDecorDef, Geom.RectJson {
-    type: "cuboid";
-    baseY: number;
-    height3d: number;
-    transform?: Geom.SixTuple;
-  }
-
-  interface DecorPoint extends BaseDecor, Geom.VectJson {
-    type: "point";
-    /** Orientation in degrees, where the unit vector `(1, 0)` corresponds to `0`  */
-    orient: number;
-    meta: Meta<Geomorph.GmRoomId & { img?: Key.DecorImg }>;
-  }
-  interface DecorPointDef extends BaseDecorDef, Geom.VectJson {
-    type: "point";
-    img?: Key.DecorImg;
-    /** Orientation in degrees, where the unit vector `(1, 0)` corresponds to `0`  */
-    orient?: number;
-    /** Height off ground */
-    y3d?: number;
-  }
-
-  /** Simple polygon sans holes. */
-  interface DecorQuad extends BaseDecor {
-    type: "quad";
-    transform: Geom.SixTuple;
-    center: Geom.VectJson;
-    /** Determinant of 2x2 part of `transform` */
-    det: number;
-    meta: Meta<Geomorph.GmRoomId & { img: Key.DecorImg }>;
-  }
-  interface DecorQuadDef extends BaseDecorDef, Geom.RectJson {
-    type: "quad";
-    /** For monochromatic use `"colour-white"` and set `color` option. */
-    img: Key.DecorImg;
-    /** three.js colour rep */
-    color?: string;
-    /** Applied before translation `x`, `y` */
-    transform?: Geom.SixTuple;
-    /** Height off ground */
-    y3d?: number;
-  }
-
-  type DecorDecal = Omit<DecorQuad, "type"> & {
-    type: "decal";
-  };
-
-  interface DecorRect extends BaseDecor {
-    type: "rect";
-    points: Geom.VectJson[];
-    /** Center of `new Poly(points)` */
-    center: Geom.VectJson;
-    /** Radians; makes an `Geom.AngledRect` together with `bounds2d`  */
-    angle: number;
-  }
-  interface DecorRectDef extends BaseDecorDef, Geom.RectJson {
-    type: "rect";
-    /** Radians; makes an `Geom.AngledRect` together with `bounds2d`  */
-    angle?: number;
-  }
-
-  interface BaseDecor {
-    /** Either auto-assigned e.g. decor from geomorphs, or specified by user. */
-    key: string;
-    meta: Meta<Geomorph.GmRoomId>;
-    /** 2D bounds inside XZ plane */
-    bounds2d: Geom.RectJson;
-    /** Epoch ms when last updated (overwritten) */
-    updatedAt?: number;
-    /**
-     * Indicates decor that comes from a geomorph layout,
-     * i.e. decor that is initially instantiated.
-     */
-    src?: Key.Geomorph;
-    // /** For defining decor via CLI (more succinct) */
-    // tags?: string[];
-  }
-  interface BaseDecorDef {
-    key: string;
-    meta?: Meta;
-  }
+  type DecorPoint = Extract<Decor, { type: "point" }>;
+  type DecorCuboid = Extract<Decor, { type: "cuboid" }>;
+  type DecorRect = Extract<Decor, { type: "rect" }>;
+  type DecorCircle = Extract<Decor, { type: "circle" }>;
+  type DecorQuad = Extract<Decor, { type: "quad" }>;
+  type DecorDecal = Extract<Decor, { type: "decal" }>;
 
   type DecorSheetRectCtxt = Meta<{
-    decorImgKey: Key.DecorImg;
+    decorImgKey: string;
     /** 0-based index of sheet */
     sheetId: number;
   }>;
