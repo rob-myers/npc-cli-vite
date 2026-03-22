b151bd3f9264c314eff9e75c176524cd1b951cba
rob-myers <me.robmyers@gmail.com>
2026-03-22 17:22:45 +0000

    feat: apply demo texture array contents to demo Floor instancedMesh

--- packages/ui/world/src/components/Floor.tsx
+++ packages/ui/world/src/components/Floor.tsx
@@ -1,12 +1,17 @@
 import { useComposedRefs, useStateRef } from "@npc-cli/util";
-import * as THREE from "three/webgpu";
+import { useContext } from "react";
+import type * as THREE from "three/webgpu";
 import { createXzQuad } from "../service/geometry";
-import { demoInstancedQuad } from "./Demo";
+import { createDemoTexArrayMaterial, demoInstancedQuad } from "./demo";
+import { WorldContext } from "./world-context";
 
 export default function Floor() {
+  const w = useContext(WorldContext);
+
   const state = useStateRef(() => ({
     inst: null as null | THREE.InstancedMesh,
     quad: createXzQuad(),
+    material: createDemoTexArrayMaterial(w.texFloor),
   }));
 
   return (
@@ -16,9 +21,8 @@ export default function Floor() {
         ref={useComposedRefs(state.ref("inst"), demoInstancedQuad.ref)}
         args={[state.quad, undefined, demoInstancedQuad.metas.length]}
         renderOrder={-3}
-      >
-        <meshBasicMaterial side={THREE.DoubleSide} />
-      </instancedMesh>
+        material={state.material}
+      />
     </group>
   );
 }
