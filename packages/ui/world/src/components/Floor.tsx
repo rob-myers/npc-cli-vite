9912e9cf337ab2fe987d90baeb24bb6bc187ca17
rob-myers <me.robmyers@gmail.com>
2026-03-23 13:52:29 +0000

    feat: debugging transformed instances

--- packages/ui/world/src/components/Floor.tsx
+++ packages/ui/world/src/components/Floor.tsx
@@ -1,8 +1,7 @@
-import { useComposedRefs, useStateRef } from "@npc-cli/util";
-import { useContext } from "react";
-import type * as THREE from "three/webgpu";
-import { createXzQuad } from "../service/geometry";
-import { createTestOutlineTexArrayMaterial, demoInstancedQuad } from "./demo";
+import { Mat, useStateRef } from "@npc-cli/util";
+import { useContext, useEffect } from "react";
+import * as THREE from "three/webgpu";
+import { createXzQuad, embedXZMat4 } from "../service/geometry";
 import { WorldContext } from "./world-context";
 
 export default function Floor() {
@@ -12,15 +11,37 @@ export default function Floor() {
     inst: null as null | THREE.InstancedMesh,
     quad: createXzQuad(),
     // material: createDemoTexArrayMaterial(w.texFloor),
-    material: createTestOutlineTexArrayMaterial(w.texFloor),
+    // material: createTestOutlineTexArrayMaterial(w.texFloor),
+    material: new THREE.MeshBasicNodeMaterial({ side: THREE.DoubleSide, wireframe: true }),
+
+    transformInstances() {
+      if (!state.inst) return;
+      for (const [gmId, gm] of w.gms.entries()) {
+        const mat = new Mat([gm.bounds.width, 0, 0, gm.bounds.height, gm.bounds.x, gm.bounds.y]).postMultiply(
+          gm.matrix,
+        );
+        // if (mat.determinant < 0) mat.preMultiply([-1, 0, 0, 1, 1, 0])
+        state.inst.setMatrixAt(gmId, embedXZMat4(mat));
+      }
+      state.inst.instanceMatrix.needsUpdate = true;
+      state.inst.computeBoundingSphere();
+    },
   }));
+
+  useEffect(() => {
+    state.transformInstances();
+    // state.addUvs();
+    // state.draw().then(() => w.update());
+  }, [w.gms.length]);
 
   return (
     <group>
       <instancedMesh
         name="floor"
-        ref={useComposedRefs(state.ref("inst"), demoInstancedQuad.ref)}
-        args={[state.quad, undefined, demoInstancedQuad.metas.length]}
+        // ref={useComposedRefs(state.ref("inst"), demoInstancedQuad.ref)}
+        // args={[state.quad, undefined, demoInstancedQuad.metas.length]}
+        ref={state.ref("inst")}
+        args={[state.quad, undefined, w.gms.length]}
         renderOrder={-3}
         material={state.material}
       />
