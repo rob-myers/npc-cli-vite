92f52aeef599f952c5bb73704a87cef0e7c49e0c
rob-myers <me.robmyers@gmail.com>
2026-03-23 10:39:48 +0000

    fix: ensure initial npc render when world tab initially hidden

--- packages/ui/world/src/components/NPCs.tsx
+++ packages/ui/world/src/components/NPCs.tsx
@@ -1,6 +1,15 @@
+import { useStateRef } from "@npc-cli/util";
+import { useContext, useEffect } from "react";
 import { SkinnedMeshTemplateDemo } from "./demo";
+import { WorldContext } from "./world-context";
 
 export default function NPCs() {
+  const w = useContext(WorldContext);
+  const state = useStateRef(() => ({}));
+
+  // ensure initial render on show hidden World tab
+  useEffect(() => void (w.view?.canvas && state.update()), [w.view?.canvas]);
+
   return (
     <group>
       <SkinnedMeshTemplateDemo />
