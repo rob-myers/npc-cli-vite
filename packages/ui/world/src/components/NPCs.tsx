5a60d19a9a3e7aaa50d96779ae9b2c18eeb60abe
rob-myers <me.robmyers@gmail.com>
2026-03-23 12:32:03 +0000

    feat: remove fix which does not work

--- packages/ui/world/src/components/NPCs.tsx
+++ packages/ui/world/src/components/NPCs.tsx
@@ -1,14 +1,11 @@
 import { useStateRef } from "@npc-cli/util";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { SkinnedMeshTemplateDemo } from "./demo";
 import { WorldContext } from "./world-context";
 
 export default function NPCs() {
-  const w = useContext(WorldContext);
-  const state = useStateRef(() => ({}));
-
-  // ensure initial render on show hidden World tab
-  useEffect(() => void (w.view?.canvas && state.update()), [w.view?.canvas]);
+  const _w = useContext(WorldContext);
+  const _state = useStateRef(() => ({}));
 
   return (
     <group>
