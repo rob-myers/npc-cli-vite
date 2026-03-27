2fd9ccf2cd62ea5ce27b5c10ed9955f93c522e52
rob-myers <me.robmyers@gmail.com>
2026-03-27 17:03:47 +0000

    feat: can request and receive navMesh from worker

--- packages/ui/world/src/world-worker.d.ts
+++ packages/ui/world/src/world-worker.d.ts
@@ -1,5 +1,5 @@
 declare namespace WW {
   type MsgToWorker = { type: "ping" } | { type: "test-generate-tiled-navmesh" };
 
-  type MsgFromWorker = { type: "pong" };
+  type MsgFromWorker = { type: "pong" } | { type: "test-generate-tiled-navmesh-result"; data: any };
 }
