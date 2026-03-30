7f1a36a78004f639520f031af8975dd7053f83ff
rob-myers <me.robmyers@gmail.com>
2026-03-30 10:08:30 +0100

    feat: reorg starship-symbol exports

---
+++ packages/media/src/starship-symbol/schema.ts
@@ -0,0 +1,40 @@
+import { keys } from "@npc-cli/util/legacy/generic";
+import z from "zod";
+import { geomorphKeys, symbolByGroup } from "./const";
+
+export const StarShipSymbolImageKeySchema = z.literal(Object.values(symbolByGroup).flatMap((group) => keys(group)));
+export type StarshipSymbolImageKey = z.infer<typeof StarShipSymbolImageKeySchema>;
+
+export const StarShipGeomorphKeySchema = z.literal(geomorphKeys);
+export type StarShipGeomorphKey = z.infer<typeof StarShipGeomorphKeySchema>;
+
+export function isHullSymbolImageKey(imageKey: StarshipSymbolImageKey): imageKey is StarShipGeomorphKey {
+  return imageKey.startsWith("g-");
+}
+
+type ExtractGeomorphNumber<T extends string> = T extends `g-${infer N}--${string}`
+  ? N extends `${infer D extends number}`
+    ? D
+    : never
+  : never;
+export type StarshipGeomorphNumber = ExtractGeomorphNumber<StarShipGeomorphKey>;
+export const StarShipGeomorphNumberSchema = z.literal(
+  [...keys(symbolByGroup["geomorph-core"]), ...keys(symbolByGroup["geomorph-edge"])].map(getGeomorphNumber),
+);
+/** g-101--multipurpose -> g-101 -> 101 */
+export function getGeomorphNumber(gmKey: StarShipGeomorphKey): StarshipGeomorphNumber {
+  return Number(gmKey.split("--")[0].slice(2)) as StarshipGeomorphNumber;
+}
+
+export const StarshipSymbolPngsManifestSchema = z.object({
+  modifiedAt: z.string(),
+  byKey: z.record(
+    StarShipSymbolImageKeySchema,
+    z.object({
+      group: z.literal(keys(symbolByGroup)),
+      width: z.number(),
+      height: z.number(),
+    }),
+  ),
+});
+export type StarshipSymbolPngsManifest = z.infer<typeof StarshipSymbolPngsManifestSchema>;
