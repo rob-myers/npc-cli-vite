6c626ba999596d4c080d4d5615092ea0556ac50e
rob-myers <me.robmyers@gmail.com>
2026-04-04 00:53:35 +0100

    feat: debug silent fail

--- packages/ui/world/src/components/World.tsx
+++ packages/ui/world/src/components/World.tsx
@@ -50,13 +50,13 @@ export default function World({ meta }: { meta: WorldUiMeta }) {
       hash: 0,
       texFloor: new TexArray({
         ctKey: "floor-tex",
-        numTextures: 1, // can change
+        numTextures: 2, // can change
         width: floorTextureDimension,
         height: floorTextureDimension,
       }),
       texCeil: new TexArray({
         ctKey: "ceil-tex",
-        numTextures: 1, // can change
+        numTextures: 2, // can change
         width: floorTextureDimension,
         height: floorTextureDimension,
       }),
@@ -188,9 +188,9 @@ export default function World({ meta }: { meta: WorldUiMeta }) {
       state.hash = hashJson(state.assets);
 
       // 🚧 debug: try fix numTextures
-      const dimension = floorTextureDimension;
-      state.texFloor.resize({ width: dimension, height: dimension, numTextures: 2 });
-      state.texCeil.resize({ width: dimension, height: dimension, numTextures: 2 });
+      // const dimension = floorTextureDimension;
+      // state.texFloor.resize({ width: dimension, height: dimension, numTextures: 2 });
+      // state.texCeil.resize({ width: dimension, height: dimension, numTextures: 2 });
 
       return null;
     },
