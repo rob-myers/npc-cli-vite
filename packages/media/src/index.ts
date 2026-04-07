b334d9dfadcf871b229f4fda69f7b6d94153c79d
rob-myers <me.robmyers@gmail.com>
2026-04-07 15:14:54 +0100

    feat: towards basic shadow in template.test-0

--- packages/media/src/index.ts
+++ packages/media/src/index.ts
@@ -1,10 +1,12 @@
 import templateGltf from "./blockbench/template.gltf?url";
-// general
+// 🖼️ general
 // import templateTexture from "./blockbench/testing/fe99881948399dca.png?url";
-// medic
+// 🖼️ medic
 import templateTexture from "./blockbench/testing/3573d1c9cad6c8fc.png?url";
+import templateTest0Gltf from "./blockbench/testing/template.test-0.gltf?url";
 
 export const url = {
   templateGltf,
+  templateTest0Gltf,
   templateTexture,
 };
