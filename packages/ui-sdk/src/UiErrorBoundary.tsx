7dd9df84094510c8302013309c72c977bdb620ae
rob-myers <me.robmyers@gmail.com>
2026-04-09 12:40:01 +0100

    feat: error boundary has refresh button

--- packages/ui-sdk/src/UiErrorBoundary.tsx
+++ packages/ui-sdk/src/UiErrorBoundary.tsx
@@ -1,12 +1,14 @@
 import type { UiInstanceMeta } from "@npc-cli/ui-sdk";
+import { cn } from "@npc-cli/util";
 import { jsStringify } from "@npc-cli/util/legacy/generic";
 import { Component } from "react";
+import { uiClassName } from "./const";
 
 export class UiErrorBoundary extends Component<
-  React.PropsWithChildren<{ meta: UiInstanceMeta }>,
+  React.PropsWithChildren<BaseProps>,
   { error: Error | typeof NoErrorSymbol }
 > {
-  constructor(props: React.PropsWithChildren<{ meta: UiInstanceMeta }>) {
+  constructor(props: React.PropsWithChildren<BaseProps>) {
     super(props);
     this.state = { error: NoErrorSymbol };
   }
@@ -22,19 +24,31 @@ export class UiErrorBoundary extends Component<
     return (
       <div className="flex flex-col gap-1 text-on-background size-full bg-black">
         <h2 className="p-4 border-b text-white border-on-background/25 bg-[repeating-linear-gradient(45deg,var(--pattern-fg)_0,var(--pattern-fg)_1px,transparent_0,transparent_50%)] bg-[size:10px_10px] bg-fixed [--pattern-fg:color-mix(in_oklch,var(--color-white)_20%,transparent)]">
-          UI Error ({this.props.meta?.uiKey})
+          UI Error: <span className="text-amber-200">{this.props.meta?.uiKey}</span>
         </h2>
         <div className="overflow-auto">
-          <pre className="px-4 py-2 whitespace-pre-wrap font-sans text-sm text-orange-600 leading-relaxed tracking-wide">
+          <pre className="px-4 py-2 whitespace-pre-wrap font-sans text-sm text-red-400 leading-relaxed tracking-wide">
             {this.state.error.message}
           </pre>
           <pre className="px-4 py-2 whitespace-pre-wrap font-mono text-xs text-amber-200 leading-relaxed tracking-wide">
             {jsStringify(this.props.meta)}
           </pre>
         </div>
+        <button
+          type="button"
+          className={cn(
+            uiClassName,
+            "cursor-pointer p-2 m-4 self-start font-sans text-sm text-black bg-white border rounded",
+          )}
+          onClick={() => this.setState({ error: NoErrorSymbol })}
+        >
+          Refresh
+        </button>
       </div>
     );
   }
 }
 
 const NoErrorSymbol = Symbol();
+
+type BaseProps = { meta: UiInstanceMeta };
