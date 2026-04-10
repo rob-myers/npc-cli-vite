9d7b2af18357ee2bc2cd361f7344e4215675f22a
rob-myers <me.robmyers@gmail.com>
2026-04-10 12:43:13 +0100

    feat: default map is not empty

--- packages/ui/world/src/schema.ts
+++ packages/ui/world/src/schema.ts
@@ -2,13 +2,14 @@ import { BaseUiMetaSchema } from "@npc-cli/ui-sdk/schema";
 import z from "zod";
 
 // Avoid HMR issue
-const emptyMapKey: typeof import("./const").emptyMapKey = "empty-map";
+// const emptyMapKey: typeof import("./const").emptyMapKey = "empty-map";
+const defaultMapKey: typeof import("./const").defaultMapKey = "small-map-0";
 
 export const WorldUiSchema = z.object({
   ...BaseUiMetaSchema.shape,
   disabled: z.boolean().default(true),
   worldKey: z.templateLiteral(["world-", z.number()]),
-  mapKey: z.string().default(emptyMapKey),
+  mapKey: z.string().default(defaultMapKey),
   // prefer left because performance tool on right
   menuPosition: BaseUiMetaSchema.shape.menuPosition.default("left"),
 });
