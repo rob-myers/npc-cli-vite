8837f58974725224ec09ff926c4d2c5dd1038ee7
rob-myers <me.robmyers@gmail.com>
2026-04-10 10:45:44 +0100

    feat: restyle error boundary

--- packages/ui-sdk/src/UiErrorBoundary.tsx
+++ packages/ui-sdk/src/UiErrorBoundary.tsx
@@ -1,6 +1,6 @@
 import type { UiInstanceMeta } from "@npc-cli/ui-sdk";
 import { cn } from "@npc-cli/util";
-import { jsStringify } from "@npc-cli/util/legacy/generic";
+import { safeJsonCompact } from "@npc-cli/util/legacy/generic";
 import { Component } from "react";
 import { uiClassName } from "./const";
 
@@ -23,22 +23,22 @@ export class UiErrorBoundary extends Component<
     }
     return (
       <div className="flex flex-col gap-1 text-on-background size-full bg-black">
-        <h2 className="p-4 border-b text-white border-on-background/25 bg-[repeating-linear-gradient(45deg,var(--pattern-fg)_0,var(--pattern-fg)_1px,transparent_0,transparent_50%)] bg-[size:10px_10px] bg-fixed [--pattern-fg:color-mix(in_oklch,var(--color-white)_20%,transparent)]">
-          UI Error: <span className="text-amber-200">{this.props.meta?.uiKey}</span>
+        <h2 className="p-4 border-b text-white-200 border-on-background/25 bg-[repeating-linear-gradient(45deg,var(--pattern-fg)_0,var(--pattern-fg)_1px,transparent_0,transparent_50%)] bg-[size:10px_10px] bg-fixed [--pattern-fg:color-mix(in_oklch,var(--color-white)_20%,transparent)]">
+          Error in <span className="font-mono">{this.props.meta?.uiKey}</span>
         </h2>
         <div className="overflow-auto">
           <pre className="px-4 py-2 whitespace-pre-wrap font-sans text-sm text-red-400 leading-relaxed tracking-wide">
             {this.state.error.message}
           </pre>
           <pre className="px-4 py-2 whitespace-pre-wrap font-mono text-xs text-amber-200 leading-relaxed tracking-wide">
-            {jsStringify(this.props.meta)}
+            {safeJsonCompact(this.props.meta)}
           </pre>
         </div>
         <button
           type="button"
           className={cn(
             uiClassName,
-            "cursor-pointer p-2 m-4 self-start font-sans text-sm text-black bg-white border rounded",
+            "cursor-pointer p-2 m-4 self-start font-sans text-sm text-white bg-black border rounded",
           )}
           onClick={() => this.setState({ error: NoErrorSymbol })}
         >
