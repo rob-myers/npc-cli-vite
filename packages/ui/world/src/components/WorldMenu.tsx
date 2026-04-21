e2ac660fe7213e41ba73eef98adfc11f90c7eb3e
rob-myers <me.robmyers@gmail.com>
2026-04-21 16:22:07 +0100

    feat: grayscale Stats while w.disabled

--- packages/ui/world/src/components/WorldMenu.tsx
+++ packages/ui/world/src/components/WorldMenu.tsx
@@ -255,7 +255,7 @@ export function WorldMenu() {
                 className="flex items-center gap-2 px-2 py-1 text-xs text-slate-300 hover:bg-slate-700 cursor-pointer"
                 closeOnClick={false}
                 onClick={() => {
-                  w.npc?.remove(...Object.keys(w.npc.npc));
+                  w.npc.remove(...Object.keys(w.npc.npc));
                   w.view.forceRender();
                 }}
               >
