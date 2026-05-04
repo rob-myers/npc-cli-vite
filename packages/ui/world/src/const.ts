7d2c82dfaee9dc492d6b2c368c78d23ffbec4bf8
rob-myers <me.robmyers@gmail.com>
2026-05-04 14:22:31 +0100

    fea: larger npc

--- packages/ui/world/src/const.ts
+++ packages/ui/world/src/const.ts
@@ -87,7 +87,7 @@ export const MAX_OBSTACLE_QUAD_INSTANCES = 1024;
 
 export const MAX_DECOR_QUAD_INSTANCES = 512;
 
-export const npcScale = 0.6;
+export const npcScale = 0.65;
 
 export const MAX_NPCS = 256;
 
