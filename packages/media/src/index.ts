4413c1933d3bc8ee279e6ecaf3ab1a12035a59bf
rob-myers <me.robmyers@gmail.com>
2026-06-04 13:19:49 +0100

    feat: improve sit animation using mcp

--- packages/media/src/index.ts
+++ packages/media/src/index.ts
@@ -1,18 +1,17 @@
-import templateGltf from "./blockbench/template/template.gltf?url";
-import extraRootThinnerGltf from "./blockbench/with-extra-root//extra-root.thinner.gltf?url";
-import templateExtraRootGltf from "./blockbench/with-extra-root/template.extra-root.gltf?url";
+// import extraRootThinnerGltf from "./blockbench/with-extra-root/extra-root.thinner.gltf?url";
+// import templateExtraRootGltf from "./blockbench/with-extra-root/template.extra-root.gltf?url";
+
 import templateMoreAnimsGltf from "./blockbench/with-extra-root/thinner.more-anims.gltf?url";
+import templateMoreAnimsWipGltf from "./blockbench/with-extra-root/thinner.more-anims.wip.gltf?url";
 
 export const url = {
-  templateGltf,
   /**
-   * Same as 'template' but with extra root "skeleton-root",
+   * Extra root is same as 'template' but with extra root "skeleton-root",
    * > root -> skeleton-root -> ...
    *
    * This permits us to attach a shadow quad and label to "root",
    * without them being affected by the skeleton's animation.
    */
-  templateExtraRootGltf,
-  extraRootThinnerGltf,
   templateMoreAnimsGltf,
+  templateMoreAnimsWipGltf,
 };
