09d42a1d2728f13fe30e6511a3437c51b161432a
rob-myers <me.robmyers@gmail.com>
2026-06-05 09:26:42 +0100

    feat: avoid slide on arrive when `move fast`

--- packages/ui/world/src/components/NPCs.tsx
+++ packages/ui/world/src/components/NPCs.tsx
@@ -252,7 +252,11 @@ export default function NPCs() {
           if (stuck === true) {
             npc.startIdle();
           } else if (
-            crowdApi.isAgentAtTarget(state.crowd, npc.agentId, npc.arrive ? 0.15 : npc.running ? 0.8 : 0.4) === true
+            crowdApi.isAgentAtTarget(
+              state.crowd,
+              npc.agentId,
+              npc.arrive ? (npc.running ? 0.025 : 0.15) : npc.running ? 0.8 : 0.4,
+            ) === true
           ) {
             // arrived
             npc.startIdle();
