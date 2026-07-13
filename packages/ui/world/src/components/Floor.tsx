7adcf1e43b6810b6ccf7d6f061df72d1fd5ed3de
rob-myers <me.robmyers@gmail.com>
2026-07-13 21:27:09 +0100

    feat: windows with meta.curved don't have seg-shadow

--- packages/ui/world/src/components/Floor.tsx
+++ packages/ui/world/src/components/Floor.tsx
@@ -92,7 +92,7 @@ export default function Floor() {
 
         // door shadow
         // 🔔 gaps in doorways should be fixed by adjusting door relative to wall
-        for (const door of layout.doors.concat(layout.windows)) {
+        for (const door of layout.doors.concat(layout.windows.filter((x) => x.meta.curved !== true))) {
           ct.lineWidth = door.meta.hull ? 0.225 : 0.125;
           tmpPoly.outline = door.seg;
           ct.lineJoin = "miter";
