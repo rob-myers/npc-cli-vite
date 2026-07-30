659ed00ce0382f3fc27547588743f84767006bd0
rob-myers <me.robmyers@gmail.com>
2026-07-30 12:46:44 +0100

    feat: remove ui/demo

--- packages/ui/demo/src/schema.ts
+++
@@ -1,8 +0,0 @@
-import { BaseUiMetaSchema } from "@npc-cli/ui-sdk/schema";
-import z from "zod";
-
-export const TemplateUiMetaSchema = z.object({
-  ...BaseUiMetaSchema.shape,
-});
-
-export type TemplateUiMeta = z.infer<typeof TemplateUiMetaSchema>;
