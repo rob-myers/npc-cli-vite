dc24346f14847fddb058fb54f181d6b460077587
rob-myers <me.robmyers@gmail.com>
2026-08-04 13:23:00 +0100

    feat: can zoom closer

--- packages/ui/world/src/components/WorldView.tsx
+++ packages/ui/world/src/components/WorldView.tsx
@@ -69,7 +69,7 @@ export function WorldView(props: React.PropsWithChildren<{ className?: string }>
         maxAzimuthAngle: +Infinity,
         minPolarAngle: Math.PI / 64,
         maxPolarAngle: Math.PI / 2 - Math.PI / 8,
-        minDistance: 16,
+        minDistance: 14,
         maxDistance: 20,
         extraZoom: 2,
         panSpeed: 2,
