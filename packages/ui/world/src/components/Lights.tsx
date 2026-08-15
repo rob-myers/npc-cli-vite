e6b044748128a4affb4344e476bf5b2ab9898711
rob-myers <me.robmyers@gmail.com>
2026-08-15 14:00:52 +0100

    feat: try brighter ambientIntensity 1 + blue tint

--- packages/ui/world/src/components/Lights.tsx
+++ packages/ui/world/src/components/Lights.tsx
@@ -3,13 +3,13 @@ import type { Vec3 } from "navcat";
 export default function Lights() {
   return (
     <group name="lights">
-      <ambientLight intensity={0.45} color="#fff" />
+      <ambientLight intensity={ambientLightIntensity} color="#fff" />
 
       <directionalLight
         position={[Math.cos(2 * Math.PI * (0 / 3)), 1, Math.sin(2 * Math.PI * (0 / 3))]}
         lookAt={origin}
         intensity={directionLightIntensity}
-        // color="red"
+        color="blue"
       />
       <directionalLight
         position={[Math.cos(2 * Math.PI * (1 / 3)), 1, Math.sin(2 * Math.PI * (1 / 3))]}
@@ -27,3 +27,4 @@ export default function Lights() {
 
 const origin: Vec3 = [0, 0, 0];
 const directionLightIntensity = 0.15;
+const ambientLightIntensity = 1;
