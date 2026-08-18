c63896dad0209ce387356bd333a98b40cd418c7b
rob-myers <me.robmyers@gmail.com>
2026-08-18 07:33:50 +0100

    feat: add jobs-0 to default ui

--- packages/ui-registry/src/default-ui.ts
+++ packages/ui-registry/src/default-ui.ts
@@ -1,4 +1,5 @@
 import type { ProfileKey } from "@npc-cli/cli/jsh/profiles";
+import Jobs from "@npc-cli/ui__jobs";
 import Jsh from "@npc-cli/ui__jsh";
 import MapEdit from "@npc-cli/ui__map-edit";
 import Tabs from "@npc-cli/ui__tabs";
@@ -11,9 +12,12 @@ import { isTouchDevice } from "@npc-cli/util/legacy/dom";
 export function getDefaultTabs() {
   const uid = () => `ui-${crypto.randomUUID()}`;
 
+  const jobsKey = "jobs-0";
+  const profileKey: ProfileKey = "world_profile_v0";
   const ttyKey = "tty-0";
   const worldKey = "world-0";
-  const profileKey: ProfileKey = "world_profile_v0";
+
+  const jobsMeta = Jobs.schema.decode({ id: uid(), title: jobsKey, uiKey: "Jobs" });
 
   const jshMeta = Jsh.schema.decode({
     id: uid(),
@@ -29,13 +33,13 @@ export function getDefaultTabs() {
     },
   });
 
-  const worldMeta = World.schema.decode({ id: uid(), title: worldKey, uiKey: "World", worldKey });
-
   const mapEditMeta = MapEdit.schema.decode({
     id: uid(),
     title: "map-edit-0",
     uiKey: "MapEdit",
   });
+
+  const worldMeta = World.schema.decode({ id: uid(), title: worldKey, uiKey: "World", worldKey });
 
   if (isTouchDevice()) {
     // only one Tabs on mobile
@@ -43,15 +47,16 @@ export function getDefaultTabs() {
       id: uid(),
       title: "tabs-0",
       uiKey: "Tabs",
-      items: [worldMeta.id, jshMeta.id, mapEditMeta.id],
+      items: [worldMeta.id, jshMeta.id, jobsMeta.id, mapEditMeta.id],
       currentTabId: worldMeta.id,
     });
     worldMeta.parentId = tabsMeta.id;
     jshMeta.parentId = tabsMeta.id;
+    jobsMeta.parentId = tabsMeta.id;
     mapEditMeta.parentId = tabsMeta.id;
     return {
       tabs: [tabsMeta],
-      toUi: Object.fromEntries([jshMeta, worldMeta, mapEditMeta, tabsMeta].map((meta) => [meta.id, meta])),
+      toUi: Object.fromEntries([jshMeta, jobsMeta, worldMeta, mapEditMeta, tabsMeta].map((meta) => [meta.id, meta])),
     };
   } else {
     const tabs0Meta = Tabs.schema.decode({
@@ -67,15 +72,18 @@ export function getDefaultTabs() {
       id: uid(),
       title: "tabs-1",
       uiKey: "Tabs",
-      items: [jshMeta.id, mapEditMeta.id],
+      items: [jshMeta.id, jobsMeta.id, mapEditMeta.id],
       currentTabId: jshMeta.id,
     });
-    jshMeta.parentId = tabs0Meta.id;
-    mapEditMeta.parentId = tabs0Meta.id;
+    jshMeta.parentId = tabs1Meta.id;
+    jobsMeta.parentId = tabs1Meta.id;
+    mapEditMeta.parentId = tabs1Meta.id;
 
     return {
       tabs: [tabs0Meta, tabs1Meta],
-      toUi: Object.fromEntries([jshMeta, worldMeta, mapEditMeta, tabs0Meta, tabs1Meta].map((meta) => [meta.id, meta])),
+      toUi: Object.fromEntries(
+        [jshMeta, jobsMeta, worldMeta, mapEditMeta, tabs0Meta, tabs1Meta].map((meta) => [meta.id, meta]),
+      ),
     };
   }
 }
