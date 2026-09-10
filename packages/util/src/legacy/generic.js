b74d1795cba2ef6260bc3c5a0ece1f8696237ba4
rob-myers <me.robmyers@gmail.com>
2026-09-10 12:37:09 +0100

    fix: function suppression caused 'function' key so omit instead

--- packages/util/src/legacy/generic.js
+++ packages/util/src/legacy/generic.js
@@ -338,9 +338,7 @@ export function jsStringify(input, pretty = false, suppressFunctions = false) {
           return "{ /* Promise */ }";
         }
         if (suppressFunctions === true && typeof value === "function") {
-          //return `function () { /* Function ${value.name} */ }`;
-          //return `function ${(value.name + 'Mock').replace(/\./g, '_')}() {}`;
-          return `function () { /* Mock Function ${value.name} */ }`;
+          return undefined;
         }
         if (typeof value?._internalRoot === "object") {
           return undefined;
