e20600d35ad10911c39f1cfe7d3107d687399774
rob-myers <me.robmyers@gmail.com>
2026-09-12 12:40:04 +0100

    feat: darker npcs i.e. brightness 0.2

--- packages/ui/world/src/components/npc.ts
+++ packages/ui/world/src/components/npc.ts
@@ -373,7 +373,7 @@ export class Npc {
       passFilter: (nodeRef, navMesh) => this.canPassNode(nodeRef, navMesh, false),
     };
 
-    this.brightness.value = 0.4;
+    this.brightness.value = 0.2;
   }
 
   isFading() {
