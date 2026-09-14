228d236c00b4dcc85addcb7f65c9242ad6d799e9
rob-myers <me.robmyers@gmail.com>
2026-09-14 14:04:23 +0100

    chore: move unused 3d models

--- packages/media/src/index.ts
+++ packages/media/src/index.ts
@@ -1,7 +1,7 @@
 // import extraRootThinnerGltf from "./blockbench/with-extra-root/extra-root.thinner.gltf?url";
 // import templateExtraRootGltf from "./blockbench/with-extra-root/template.extra-root.gltf?url";
+// import templateMoreAnimsGltf from "./blockbench/with-extra-root/thinner.more-anims.gltf?url";
 
-import templateMoreAnimsGltf from "./blockbench/with-extra-root/thinner.more-anims.gltf?url";
 import templateMoreAnimsWipGltf from "./blockbench/with-extra-root/thinner.more-anims.wip.gltf?url";
 
 export const url = {
@@ -12,6 +12,5 @@ export const url = {
    * This permits us to attach a shadow quad and label to "root",
    * without them being affected by the skeleton's animation.
    */
-  templateMoreAnimsGltf,
   templateMoreAnimsWipGltf,
 };
